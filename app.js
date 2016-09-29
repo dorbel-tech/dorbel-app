05f68b51b0e8ce9301a7019901cd26e2b5a7df38
David Virtser <david@dorbel.com>
2016-09-29 16:18:32 +0300

    testing new PR

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var koa = require('koa');
 var app = module.exports = koa();
 
 app.use(function *(){
-  this.body = 'Hello World';
+  this.body = 'Hello World new PR test';
 });
 
 if (!module.parent) app.listen(3000);
