e58981f27024c9305f4cefee097d7600987f84a5
David Virtser <david@dorbel.com>
2016-10-02 11:21:52 +0300

    testing new localtion

--- app.notifications.js
+++
@@ -1,8 +0,0 @@
-var koa = require('koa');
-var app = module.exports = koa();
-
-app.use(function *(){
-  this.body = 'Hello notifications app';
-});
-
-if (!module.parent) app.listen(process.env.PORT || 3001);
--- app/Dockerfile.app
+++ app/Dockerfile.app
@@ -4,7 +4,7 @@ FROM node:latest
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
 
-COPY package.json /usr/src/app/
+COPY package.json /usr/src/app
 RUN npm install --silent
 COPY . /usr/src/app
 
