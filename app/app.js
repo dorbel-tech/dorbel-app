3886fb3ef7d6cdeabb45829da30bfae5778afb41
David Virtser <david@dorbel.com>
2016-10-05 12:46:53 +0300

    more robust docker file

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ var koa = require('koa');
 var app = module.exports = koa();
 
 app.use(function *(){
-  this.body = 'Hello web app';
+  this.body = 'Hello web app yey!';
 });
 
 if (!module.parent) app.listen(process.env.PORT || 3000);
