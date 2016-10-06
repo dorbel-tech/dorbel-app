4bdb5dbe84bb37249c72a6ec21906501203316e2
Avner Sorek <avner.sorek@gmail.com>
2016-10-06 18:24:54 +0300

    Configuration for vs-code debugging

--- shared/src/config.js
+++ shared/src/config.js
@@ -10,8 +10,8 @@ function setConfigFileFolder(folder) {
   const environmentConfigFile = path.join(folder, process.env.NODE_ENV + '.json');
   const sharedConfigFile = path.join(folder, 'shared.json');
   config = config
-    .file({ file: environmentConfigFile }) // files are last in the priority
-    .file({ file: sharedConfigFile }); // shared comes last
+    .file('env', environmentConfigFile) // files are last in the priority
+    .file('shared', sharedConfigFile ); // shared comes last
 }
 
 function get(key) {
