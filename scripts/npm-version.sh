dd35370a1467afb80ef633efdf68f23f160ccf13
David Virtser <david@dorbel.com>
2016-10-18 09:10:10 +0300

    version only services

--- scripts/npm-version.sh
+++ scripts/npm-version.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # A script to install all modules dependencies.
 
-npm version $1 -m 'version bump as a result of new deployment' --force
 npm --prefix ./apartments-api version $1 -m 'version bump as a result of new deployment' --force
