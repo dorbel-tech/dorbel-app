8a2effefda4db2b52f1d4bc4a9267850fef0bc0b
David Virtser <david@dorbel.com>
2016-10-18 09:10:43 +0300

    description changed

--- scripts/npm-version.sh
+++ scripts/npm-version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# A script to install all modules dependencies.
+# A script to change version of all application services.
 
 npm --prefix ./apartments-api version $1 -m 'version bump as a result of new deployment' --force
