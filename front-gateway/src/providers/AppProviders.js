c8650bdaa8476178f1571c6d6078edf46335356f
Avner Sorek <avner.sorek@gmail.com>
2016-10-25 19:22:02 +0300

    fix auth0 config coming from dorbelConfig in client

--- front-gateway/src/providers/AppProviders.js
+++ front-gateway/src/providers/AppProviders.js
@@ -7,8 +7,8 @@ const isServer = !global.window;
 class AppProviders {
   constructor(appStore) {
     if (!isServer) {
-      if (!process.env.AUTH0_CLIENT_ID || !process.env.AUTH0_DOMAIN) throw new Error('must set auth0 env vars');
-      this.authProvider = new AuthProvider(process.env.AUTH0_CLIENT_ID, process.env.AUTH0_DOMAIN, appStore.authStore);
+      if (!dorbelConfig.AUTH0_CLIENT_ID || !dorbelConfig.AUTH0_DOMAIN) throw new Error('must set auth0 env vars');
+      this.authProvider = new AuthProvider(dorbelConfig.AUTH0_CLIENT_ID, dorbelConfig.AUTH0_DOMAIN, appStore.authStore);
     }
 
     this.apiProvider = new ApiProvider(appStore);
