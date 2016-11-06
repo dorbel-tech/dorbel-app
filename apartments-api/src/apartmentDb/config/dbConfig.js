2bf7bee6ddf261ff0b1de7e2b80decb55da1453c
David Virtser <david@dorbel.com>
2016-11-06 20:23:21 +0200

    fixed lint

--- apartments-api/src/apartmentDb/config/dbConfig.js
+++ apartments-api/src/apartmentDb/config/dbConfig.js
@@ -1,32 +1,32 @@
 // Main Sequelize CLI database configuration file.
 // In use for Sequelize CLI seeds & migrations only - https://github.com/sequelize/cli
 module.exports = {
-  "local": {
-    "username": "dorbel",
-    "password": "dorbel",
-    "database": "dorbel",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  local: {
+    username: 'dorbel',
+    password: 'dorbel',
+    database: 'dorbel',
+    host: '127.0.0.1',
+    dialect: 'mysql'
   },
-  "development": {
-    "username": process.env.RDS_USERNAME,
-    "password": process.env.RDS_PASSWORD,
-    "database": process.env.RDS_DB_NAME,
-    "host": process.env.RDS_HOSTNAME,
-    "dialect": "mysql"
+  development: {
+    username: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DB_NAME,
+    host: process.env.RDS_HOSTNAME,
+    dialect: 'mysql'
   },
-  "development": {
-    "username": process.env.RDS_USERNAME,
-    "password": process.env.RDS_PASSWORD,
-    "database": process.env.RDS_DB_NAME,
-    "host": process.env.RDS_HOSTNAME,
-    "dialect": "mysql"
+  test: {
+    username: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DB_NAME,
+    host: process.env.RDS_HOSTNAME,
+    dialect: 'mysql'
   },
-  "development": {
-    "username": process.env.RDS_USERNAME,
-    "password": process.env.RDS_PASSWORD,
-    "database": process.env.RDS_DB_NAME,
-    "host": process.env.RDS_HOSTNAME,
-    "dialect": "mysql"
-  },
+  production: {
+    username: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DB_NAME,
+    host: process.env.RDS_HOSTNAME,
+    dialect: 'mysql'
+  }
 };
