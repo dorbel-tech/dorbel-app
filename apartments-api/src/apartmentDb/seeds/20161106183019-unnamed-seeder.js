46cbc06a5848e19eaf7b8981ed06a50351e156ab
David Virtser <david@dorbel.com>
2016-11-06 20:36:49 +0200

    adding sample migration and seed scripts

---
+++ apartments-api/src/apartmentDb/seeds/20161106183019-unnamed-seeder.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Tests', [{
+      foo: 'aaa',
+      bar: 'bbb',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }], {});
+  },
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('Tests', null, {});
+  }
+};
