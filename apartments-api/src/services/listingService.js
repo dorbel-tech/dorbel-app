a907d263c453f88f1b7384a08f03b73ff89acd00
David Virtser <david@dorbel.com>
2016-11-20 18:34:09 +0200

    write errors to log

--- apartments-api/src/services/listingService.js
+++ apartments-api/src/services/listingService.js
@@ -1,21 +1,29 @@
 'use strict';
 const listingRepository = require('../apartmentDb/repositories/listingRepository');
 const moment = require('moment');
+const shared = require('dorbel-shared');
+const logger = shared.logger.getLogger(module);
 
 // TODO : errors thrown here return 500
 
 function* create(listing) {
   if (!listing.images || !listing.images.length) {
-    throw new Error('listing must contain at least one image');
+    const error1 = new Error('listing must contain at least one image');
+    logger.error(error1);
+    throw error1;
   }
 
   if (!listing.open_house_events || !listing.open_house_events.length) {
-    throw new Error('listing must contain at least one open house event');
+    const error2 = new Error('listing must contain at least one open house event');
+    logger.error(error2);
+    throw error2;
   }
 
   const existingOpenListingForApartment = yield listingRepository.getListingsForApartment(listing.apartment, { status: { $notIn: ['closed', 'rented'] } });
   if (existingOpenListingForApartment && existingOpenListingForApartment.length) {
-    throw new Error('apartment already has an active listing');
+    const error3 = new Error('apartment already has an active listing');
+    logger.error(error3);
+    throw error3;
   }
 
   if (listing.lease_start && !listing.lease_end) {
