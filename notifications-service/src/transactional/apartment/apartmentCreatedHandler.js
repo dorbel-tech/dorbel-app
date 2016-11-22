4451ea87f4527dd6547082f8c54da86e49e16357
David Virtser <david@dorbel.com>
2016-11-22 09:36:20 +0200

    better string sms management with lodash template

--- notifications-service/src/transactional/apartment/apartmentCreatedHandler.js
+++ notifications-service/src/transactional/apartment/apartmentCreatedHandler.js
@@ -2,16 +2,17 @@
 'use strict';
 const shared = require('dorbel-shared');
 const logger = shared.logger.getLogger(module);
+const _ = require('lodash');
 const emailDispatcher = require('../../dispatchers/emailDispatcher');
 const smsDispatcher = require('../../dispatchers/smsDispatcher');
 
 function send(messageType, messageBody) {
   switch (messageType) {
-    case 'Email':     
-      return sendEmail(messageBody); 
-    case 'SMS': 
-      return sendSMS(messageBody);     
-  
+    case 'Email':
+      return sendEmail(messageBody);
+    case 'SMS':
+      return sendSMS(messageBody);
+
     default:
       throw new Error('Message type wasnt defined!');
   }
@@ -25,16 +26,13 @@ function sendEmail(messageBody) {
   const additionalParams = {
     userEmail: 'david@dorbel.com', // TODO: Get user email.
     userFullName: 'Dorbel Tester', // TODO: Get user full name.
-    mergeVars: [
-      {
-        name: 'environment',
-        value: message.environemnt
-      },
-      {
-        name: 'apartment_id',
-        value: message.dataPayload.apartment_id
-      }
-    ]
+    mergeVars: [{
+      name: 'environment',
+      value: message.environemnt
+    }, {
+      name: 'apartment_id',
+      value: message.dataPayload.apartment_id
+    }]
   };
 
   return emailDispatcher.send(templateName, additionalParams);
@@ -44,7 +42,11 @@ function sendSMS(messageBody) {
   logger.debug('Sending SMS');
   const message = JSON.parse(messageBody.Message);
   const toPhoneNumber = '+972544472571'; // TODO: Get user phone number.
-  const smsText = 'Hello from notifications service with aprtment id: ' + message.dataPayload.apartment_id + ' (' + message.environemnt + ')';
+  const smsTemplate = _.template('Hello from notifications service with aprtment id: <%= apartment_id %> (<%= environemnt %>)');
+  const smsText = smsTemplate({
+    apartment_id: message.dataPayload.apartment_id,
+    environemnt: message.environemnt
+  });
 
   return smsDispatcher.send(toPhoneNumber, smsText);
 }
