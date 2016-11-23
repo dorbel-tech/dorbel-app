61361f3d5f986796ec472d5ce2523bca3766a80c
Gal Segal <gals@namogoo.com>
2016-11-23 11:32:56 +0200

    cleanup + create migration

---
+++ ohe-api/src/openHouseEventsDb/migrations/20161123100000-create-openHouseEvents.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('OpenHouseEvents', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      listing_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      start_time: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      end_time: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      is_active: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+        defaultValue: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface) {
+    return queryInterface.dropTable('OpenHouseEvents');
+  }
+};
--- ohe-api/src/openHouseEventsDb/models/open_house_event.js
+++ ohe-api/src/openHouseEventsDb/models/open_house_event.js
@@ -4,13 +4,9 @@ function define(sequelize, DataTypes) {
   return sequelize.define('open_house_event', {
     start_time: { type: DataTypes.DATE, allowNull: false },
     end_time: { type: DataTypes.DATE, allowNull: false },
-    comments:  DataTypes.TEXT
-  }, {
-    classMethods: {
-      associate: models => models.open_house_event.belongsTo(models.listing, { foreignKey: { allowNull: false }, onDelete: 'CASCADE' })
-    }
+    listing_id: { type: DataTypes.INTEGER, allowNull: false },
+    is_active: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: true }
   });
 }
 
 module.exports = define;
-
