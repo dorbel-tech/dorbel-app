e26f7bb83d8f1e8c0a20cad6f0253da33fa3f8c0
Gal Segal <gals@namogoo.com>
2016-11-29 18:03:15 +0200

    cleanup

--- ohe-api/test/shared/fakeObjectGenerator.js
+++ ohe-api/test/shared/fakeObjectGenerator.js
@@ -1,35 +1,6 @@
 'use strict';
 var faker = require('faker');
 
-function getDateString() {
-  return (new Date()).toISOString().substring(0, 10);
-}
-
-function getFakeListing() {
-  return {
-    monthly_rent: faker.random.number(),
-    lease_start: getDateString(),
-    lease_end: getDateString(),
-    publishing_user_id: faker.random.uuid(),
-    publishing_user_type: 'landlord',
-    apartment: {
-      apt_number: faker.random.number(99) + faker.random.word()[0], // like '57D'
-      rooms: 3,
-      size: 35,
-      floor: 3,
-      building: {
-        street_name: 'רוטשילד',
-        house_number: '192',
-        city: {
-          city_name: 'תל אביב'
-        }
-      }
-    },
-    images: [{ url: faker.internet.url() }],
-    open_house_events: [{ start_time: new Date(), end_time: new Date() }]
-  };
-}
-
 function getFakeUser() {
   return {
     id: faker.random.uuid(),
@@ -38,6 +9,5 @@ function getFakeUser() {
 }
 
 module.exports = {
-  getFakeListing,
   getFakeUser
 };
