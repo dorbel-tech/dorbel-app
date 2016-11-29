6b63ac36cc023d5dbb14a44da45bb81577cc1eae
David Virtser <david@dorbel.com>
2016-11-29 12:03:49 +0200

    integrating with auth0 get user details to send messages to the right person

--- notifications-service/src/transactional/apartment/apartmentCreatedHandler.js
+++ notifications-service/src/transactional/apartment/apartmentCreatedHandler.js
@@ -5,27 +5,33 @@ const logger = shared.logger.getLogger(module);
 const _ = require('lodash');
 const emailDispatcher = require('../../dispatchers/emailDispatcher');
 const smsDispatcher = require('../../dispatchers/smsDispatcher');
+const userManagement = shared.utils.userManagement;
 
 function send(messageType, messageBody) {
+  const message = JSON.parse(messageBody.Message);
+  logger.info(userManagement);
+  let userDetails = userManagement.getUserDetails(message.dataPayload.uuid);
+
   switch (messageType) {
     case 'Email':
-      return sendEmail(messageBody);
+      return sendEmail(messageBody, userDetails);
     case 'SMS':
-      return sendSMS(messageBody);
+      return sendSMS(messageBody, userDetails);
 
     default:
       throw new Error('Message type wasnt defined!');
   }
 }
 
-function sendEmail(messageBody) {
+function sendEmail(messageBody, userDetails) {
   logger.debug('Sending email');
   const message = JSON.parse(messageBody.Message);
+
   // Pass dynamic params in email body using mergeVars object.
   const templateName = 'test';
   const additionalParams = {
-    userEmail: 'david@dorbel.com', // TODO: Get user email.
-    userFullName: 'Dorbel Tester', // TODO: Get user full name.
+    userEmail: userDetails.email,
+    userFullName: userDetails.name,
     mergeVars: [{
       name: 'environment',
       content: message.environemnt
@@ -38,10 +44,10 @@ function sendEmail(messageBody) {
   return emailDispatcher.send(templateName, additionalParams);
 }
 
-function sendSMS(messageBody) {
+function sendSMS(messageBody, userDetails) {
   logger.debug('Sending SMS');
   const message = JSON.parse(messageBody.Message);
-  const toPhoneNumber = '+972544472571'; // TODO: Get user phone number.
+  const toPhoneNumber = userDetails.phone;
   const smsTemplate = _.template('Hello from notifications service with aprtment id: <%= apartment_id %> (<%= environemnt %>)');
   const smsText = smsTemplate({
     apartment_id: message.dataPayload.apartment_id,
