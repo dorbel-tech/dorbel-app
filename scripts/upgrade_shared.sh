205d08d44c09a74982659cd4e53a110ca2da6e9d
David Virtser <david@dorbel.com>
2016-11-30 09:55:51 +0200

    adding upgrade shared script

---
+++ scripts/upgrade_shared.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# A script to upgrade dorbel-shared module with new version in all projects at once.
+
+if [ $# -eq 0 ]; then
+    echo "No arguments provided."
+    echo "[version] number is a valid options."
+    exit 1
+fi
+
+if [[ $1 ]]; then
+    VERSION=v$1
+    PACKAGE_URL=dorbel-shared@git://github.com/dorbel-tech/dorbel-shared#$VERSION
+
+    cd apartments-api && yarn upgrade $PACKAGE_URL && cd ..
+    cd front-gateway && yarn upgrade $PACKAGE_URL && cd ..
+    cd notifications-service && yarn upgrade $PACKAGE_URL && cd ..
+    cd ohe-api && yarn upgrade $PACKAGE_URL && cd ..
+fi
