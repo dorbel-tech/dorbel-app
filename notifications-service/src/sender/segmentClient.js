656c23b725737f6dfe2501e62abaf90890350bda
Avner Sorek <avner.sorek@gmail.com>
2016-12-12 18:24:38 +0200

    turn segment off when there is no key

--- notifications-service/src/sender/segmentClient.js
+++ notifications-service/src/sender/segmentClient.js
@@ -1,8 +1,17 @@
 'use strict';
-var Analytics = require('analytics-node');
-var shared = require('dorbel-shared');
-var logger = shared.logger.getLogger(module);
-var analytics = new Analytics(shared.config.get('SEGMENT_IO_WRITE_KEY'));
+const Analytics = require('analytics-node');
+const shared = require('dorbel-shared');
+const logger = shared.logger.getLogger(module);
+
+const key = shared.config.get('SEGMENT_IO_WRITE_KEY');
+let analytics;
+
+if (key) {
+  analytics = new Analytics(key);
+} else {
+  logger.warn('Segment.io will not track as no key was supplied');
+  analytics = { track: () => {} };
+}
 
 function track(userId, eventName, properties) {
   logger.debug({userId, eventName, properties}, 'tracking event to segment io');
