8f85b5961680311bbc6be326fd8bbe81e74a108b
David Virtser <david@dorbel.com>
2016-12-22 18:24:49 +0200

    more little changes

--- front-gateway/test/browserstack.conf.js
+++ front-gateway/test/browserstack.conf.js
@@ -19,7 +19,7 @@ let nightwatch_config = {
         'os_version': '10',
         'browser': 'Chrome',
         'browser_version': '55.0',
-        'resolution': '1024x768'
+        'resolution': '1024x768',
         'browserstack.debug': true,
       }
     }
