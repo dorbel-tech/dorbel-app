a369d31000b767f77f7916ed9c5e539e23c757f3
Idan Langer <lxngxr@gmail.com>
2016-12-30 21:14:46 +0200

    related listings endpoint
    connects to #142

---
+++ apartments-api/src/server/controllers/relatedListingController.js
@@ -0,0 +1,14 @@
+'use strict';
+//const shared = require('dorbel-shared');
+//const logger = shared.logger.getLogger(module);
+const listingService = require('../../services/listingService');
+//const _ = require('lodash');
+
+function* get() {
+  const listingId = parseInt(this.params.listingId);
+  this.response.body = yield listingService.getRelated(listingId);
+}
+
+module.exports = {
+  get: get
+};
