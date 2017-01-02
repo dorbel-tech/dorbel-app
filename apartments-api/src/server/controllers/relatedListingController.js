14c9b4f43283fa6a1324c68111c16562b48fc1b6
Idan Langer <lxngxr@gmail.com>
2017-01-02 16:35:03 +0200

    apartments-api fix

--- apartments-api/src/server/controllers/relatedListingController.js
+++ apartments-api/src/server/controllers/relatedListingController.js
@@ -6,7 +6,27 @@ const listingService = require('../../services/listingService');
 
 function* get() {
   const listingId = parseInt(this.params.listingId);
-  this.response.body = yield listingService.getRelated(listingId, 3);
+  const listing = yield listingService.getById(listingId);
+
+  // Verify that the listing exists
+  if (listing) {
+
+    const listingQuery = {
+      status: 'listed',
+      $not: {
+        id: listingId
+      }
+    };
+
+    const options = {
+      buildingQuery: {
+        city_id: listing.apartment.building.city_id
+      },
+      limit: 3 //TODO: move to config
+    };
+
+    this.response.body = yield listingService.list(listingQuery, options);
+  }
 }
 
 module.exports = {
