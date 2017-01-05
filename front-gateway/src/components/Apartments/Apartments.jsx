7abafef3ef8e85f41069679af842f68ee2331bc5
Amos Lande <amos@dorbel.com>
2017-01-05 16:10:14 +0200

    Aligned with listing model changes

--- front-gateway/src/components/Apartments/Apartments.jsx
+++ front-gateway/src/components/Apartments/Apartments.jsx
@@ -8,39 +8,43 @@ class Apartments extends Component {
     this.props.appProviders.apartmentsProvider.loadApartments();
   }
 
-  render() {    
+  render() {
     const { listingStore } = this.props.appStore;
 
     return (
       <div>
-        {listingStore.apartments.map(apt =>
-          <div key={apt.id} class="col-lg-4 col-md-6 col-sm-6 col-xs-12 clearfix">
-            <a href="/apartments/{apt.id}" class="thumbnail search-result-container-single pull-right">
-              <div class="triangle">
-                <svg>
+        {listingStore.apartments.map(listing => {
+          var apt = listing.apartment;
+          var building = apt.building;
+          var image_url = listing.images.length > 0 ? listing.images[0].url : '';
 
-                </svg>
-                <span><i>&nbsp&nbsp</i>ריקה</span>
-              </div>
-              <div class="result-apt-image">
-                <img src={apt.image_url} alt="..."/>
-              </div>
-              <div class="search-result-apt-bottom-strip">
-                <ul>
-                  <li>{apt.monthly_rent} ₪</li>
-                  <span>|</span>
-                  <li>{apt.size} מ״ר</li>
-                  <span>|</span>
-                  <li>{apt.rooms} חד׳</li>
-                </ul>
-              </div>
-              <div class="caption">
-                <h4>{apt.description}</h4>
-                <span>{apt.street_name} {apt.house_number} - {apt.apt_number}</span>
-              </div>
-            </a>
-          </div>
-        )}
+          return  <div key={listing.id} class="col-lg-4 col-md-6 col-sm-6 col-xs-12 clearfix">
+                    <a href="/apartments/{listing.id}" class="thumbnail search-result-container-single pull-right">
+                      <div class="triangle">
+                        <svg>
+
+                        </svg>
+                        <span><i>&nbsp&nbsp</i>ריקה</span>
+                      </div>
+                      <div class="result-apt-image">
+                        <img src={image_url} alt="..."/>
+                      </div>
+                      <div class="search-result-apt-bottom-strip">
+                        <ul>
+                          <li>{listing.monthly_rent} ₪</li>
+                          <span>|</span>
+                          <li>{apt.size} מ״ר</li>
+                          <span>|</span>
+                          <li>{apt.rooms} חד׳</li>
+                        </ul>
+                      </div>
+                      <div class="caption">
+                        <h4>{listing.description}</h4>
+                        <span>{building.street_name} {building.house_number} - {apt.apt_number}</span>
+                      </div>
+                    </a>
+                  </div>
+        })}
 
         {this.props.children}
       </div>
