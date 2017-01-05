d678ca0d457b87f04278658a55d33268f165f58d
Idan Langer <lxngxr@gmail.com>
2017-01-05 14:53:29 +0200

    fix tests in 'front-gateway'

--- front-gateway/src/providers/ApartmentsProvider.spec.js
+++ front-gateway/src/providers/ApartmentsProvider.spec.js
@@ -6,9 +6,11 @@ import ApartmentsProvider from './ApartmentsProvider';
 describe('ApartmentsProvider', function () {
 
   beforeAll(function () {
-    this.mockApartments = [ 123, 456 ];
-    this.apiProviderMock = {
-      fetch: stub().resolves(this.mockApartments)
+    this.mockApartments = [123, 456];
+    this.providersMock = {
+      api: {
+        fetch: stub().resolves(this.mockApartments)
+      }
     };
     this.appStoreMock = {
       listingStore: {
@@ -16,12 +18,12 @@ describe('ApartmentsProvider', function () {
       }
     };
 
-    this.apartmentsProvider = new ApartmentsProvider(this.appStoreMock, this.apiProviderMock);
+    this.apartmentsProvider = new ApartmentsProvider(this.appStoreMock, this.providersMock);
   });
 
   it('should call API to load apartments and save them to store', function () {
     return this.apartmentsProvider.loadApartments()
-    .then(() => expect(this.appStoreMock.listingStore.add.args[0][0]).toBe(this.mockApartments));
+      .then(() => expect(this.appStoreMock.listingStore.add.args[0][0]).toBe(this.mockApartments));
   });
 
 });
