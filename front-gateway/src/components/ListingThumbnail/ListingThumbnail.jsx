b964d162719546a41536b3688a2e6cb2585af61b
Amos Lande <amoslande@users.noreply.github.com>
2017-01-10 18:07:37 +0200

    Feature/apartments filtering page (#195)
    * Initial commit - apartments design
    
    * Aligned with listing model changes
    
    * Initial commit grid layout
    
    * Basic layout fixes
    
    * Switched to using the ListingThumbnail react component
    
    * City selector initial commit
    
    * Layout
    
    * Layout and a mobx warning fix
    
    * Added react-nouislider
    
    * Slider style
    
    * Layout
    
    * No results
    
    * Lint
    
    * Change slider functionality
    
    * Linting
    
    * Linting
    
    * Fix unit test
    
    * Clean package.json
    
    * Style and room slider

--- front-gateway/src/components/ListingThumbnail/ListingThumbnail.jsx
+++ front-gateway/src/components/ListingThumbnail/ListingThumbnail.jsx
@@ -7,32 +7,33 @@ class ListingThumbnail extends Component {
   render() {
     const { listing } = this.props;
     const title = listing.title || `דירת ${listing.apartment.rooms} חד׳ ברח׳ ${listing.apartment.building.street_name}`;
+    // TODO: Make sure the image URL points to the primary image URL (by order).
+    const imageURL = listing.images.length ? listing.images[0].url : '';
 
     return (
-        <Col lg={4} md={4} sm={6}>
-            <NavLink to={'/apartments/' + listing.id} className="thumbnail apt-thumb-container-single pull-right">
-                <div className="apt-thumb-apt-image">
-                    <img src={listing.images[0] ?
-                        listing.images[0].url : ''} alt="..." />
-                </div>
-                <div className="apt-thumb-apt-bottom-strip">
-                    <ul>
-                        <li>{listing.monthly_rent} ₪</li>
-                            <span>|</span>
-                            <li>{listing.apartment.size} מ״ר</li>
-                            <span>|</span>
-                        <li>{listing.apartment.rooms} חדרים</li>
-                    </ul>
-
-                    </div>
-                    <div className="caption">
-                        <h4>{title}</h4>
-                        <span>
-                            {listing.apartment.building.street_name},{listing.apartment.building.city.city_name}
-                        </span>
-                    </div>
-                </NavLink>
-            </Col>
+      <Col lg={4} sm={6} xs={12}>
+        <NavLink to={'/apartments/' + listing.id}
+                 className="thumbnail apt-thumb-container-single pull-right">
+          <div className="apt-thumb-apt-image">
+            <img src={imageURL} />
+          </div>
+          <div className="apt-thumb-apt-bottom-strip">
+            <ul>
+              <li>{listing.monthly_rent} ₪</li>
+              <span>|</span>
+              <li>{listing.apartment.size} מ״ר</li>
+              <span>|</span>
+              <li>{listing.apartment.rooms} חדרים</li>
+            </ul>
+          </div>
+          <div className="caption">
+            <h4>{title}</h4>
+            <span>
+              {listing.apartment.building.street_name},{listing.apartment.building.city.city_name}
+            </span>
+          </div>
+        </NavLink>
+      </Col>
     );
   }
 }
