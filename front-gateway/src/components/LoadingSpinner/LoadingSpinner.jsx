5c78b63792afdacdfdb5b2dade517f24621a5dd7
Idan Langer <lxngxr@gmail.com>
2017-01-18 11:26:37 +0200

    Add loader to Apartment component

--- front-gateway/src/components/LoadingSpinner/LoadingSpinner.jsx
+++ front-gateway/src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -3,11 +3,21 @@ import Spinner from 'react-spinkit';
 import './LoadingSpinner.scss';
 
 class LoadingSpinner extends Component {
+
   render() {
+    let style = {};
+    if (this.props.size){
+      style.height = this.props.size+'px';
+      style.width = this.props.size+'px';
+    }
     return (
-        <Spinner spinnerName="circle"/>
+        <Spinner spinnerName="circle" style={style} noFadeIn />
     );
   }
 }
+
+LoadingSpinner.propTypes = {
+  size: React.PropTypes.number
+};
 
 export default LoadingSpinner;
