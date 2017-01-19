03da2448f885c5b3c989846fa2a9fda7b1ebac11
Amos Lande <amos@dorbel.com>
2017-01-19 17:45:29 +0200

    Header basic layout and auth

--- front-gateway/src/components/Header/Header.jsx
+++ front-gateway/src/components/Header/Header.jsx
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes as T } from 'react';
 import { observer } from 'mobx-react';
 import { Nav, Navbar, NavItem } from 'react-bootstrap';
 
 import './Header.scss';
 
-@observer(['router'])
+@observer(['appProviders', 'appStore', 'router'])
 class Header extends Component {
+  static propTypes = {
+    appProviders: T.object,
+    appStore: T.object
+  }
+
   routeTo(link) {
     if (this.props.router.setRoute) {
       this.props.router.setRoute(link);
@@ -13,6 +18,12 @@ class Header extends Component {
   }
 
   render() {
+    const { appProviders, appStore } = this.props;
+    const isLoggedIn = appStore.authStore.isLoggedIn ||
+      window.location.hash.indexOf('access_token') > -1;
+
+    const firstName = isLoggedIn ? appStore.authStore.getProfile().first_name : '';
+
     return (
       <Navbar className="header-navbar" collapseOnSelect fixedTop fluid inverse>
         <Navbar.Header>
@@ -25,16 +36,24 @@ class Header extends Component {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav>
-            <NavItem eventKey={1} onClick={() => this.routeTo('https://www.dorbel.com/pages/about_us')}
+            <NavItem onClick={() => this.routeTo('https://www.dorbel.com/pages/about_us')}
               href="https://www.dorbel.com/pages/about_us">מי אנחנו</NavItem>
-            <NavItem eventKey={2} onClick={() => this.routeTo('https://www.dorbel.com/pages/owner')}
+            <NavItem onClick={() => this.routeTo('https://www.dorbel.com/pages/owner')}
               href="https://www.dorbel.com/pages/owner">בעלי דירות</NavItem>
-            <NavItem eventKey={3} onClick={() => this.routeTo('https://www.dorbel.com/pages/שירותים-לבעלי-דירות')}
+            <NavItem onClick={() => this.routeTo('https://www.dorbel.com/pages/שירותים-לבעלי-דירות')}
               href="https://www.dorbel.com/pages/שירותים-לבעלי-דירות">שירותים לבעלי דירות</NavItem>
-            <NavItem eventKey={4} onClick={() => this.routeTo('/apartments')}
+            <NavItem onClick={() => this.routeTo('/apartments')}
               href="/apartments">מצאו דירה</NavItem>
-            <NavItem eventKey={5} onClick={() => this.routeTo('/apartments/new_form')}
+            <NavItem onClick={() => this.routeTo('/apartments/new_form')}
               href="/apartments/new_form">פרסמו דירה</NavItem>
+          </Nav>
+          <Nav pullLeft>
+            <NavItem>{ firstName }</NavItem>
+            {isLoggedIn ?
+              <NavItem onClick={appProviders.authProvider.logout}>התנתק</NavItem>
+              :
+              <NavItem onClick={appProviders.authProvider.showLoginModal}>התחבר</NavItem>
+            }
           </Nav>
         </Navbar.Collapse>
       </Navbar>
--- front-gateway/src/providers/AuthProvider.js
+++ front-gateway/src/providers/AuthProvider.js
@@ -82,7 +82,6 @@ class AuthProvider {
 
   logout() {
     this.authStore.logout();
-    this.router.setRoute('/');
   }
 
   reportIdentifyAnalytics(profile) {
