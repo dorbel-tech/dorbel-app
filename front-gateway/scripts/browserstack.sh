633fcd75942cefbc7aafafee2deb9b5c88ee1940
David Virtser <david@dorbel.com>
2017-01-19 15:43:54 +0200

    changes tun e2e inside container

---
+++ front-gateway/scripts/browserstack.sh
@@ -0,0 +1,15 @@
+# Installing Browserstack local testing tunnel
+# https://www.browserstack.com/local-testing#command-line
+#!/usr/bin/env bash
+
+if [ "$(uname)" == "Darwin" ]; then
+    # Do something under Mac OS X platform        
+    wget https://www.browserstack.com/browserstack-local/BrowserStackLocal-darwin-x64.zip
+    unzip BrowserStackLocal-darwin-x64.zip
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Do something under GNU/Linux platform
+    wget http://www.browserstack.com/browserstack-local/BrowserStackLocal-linux-x64.zip
+    unzip BrowserStackLocal-linux-x64.zip
+fi
+
+./BrowserStackLocal $BROWSERSTACK_KEY localhost,3001,0 > /dev/null &
