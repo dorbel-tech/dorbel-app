6e1cf86f505276121030706a45ae9fa8bac82a0b
David Virtser <david@dorbel.com>
2017-01-22 10:00:14 +0200

    better linux bash spport

--- front-gateway/scripts/browserstack.sh
+++ front-gateway/scripts/browserstack.sh
@@ -1,13 +1,15 @@
+#!/bin/bash
 # Installing Browserstack local testing tunnel
 # https://www.browserstack.com/local-testing#command-line
-#!/usr/bin/env bash
 
-if [ ! -f "./BrowserStackLocal" ]; then
-    if [ "$(uname)" == "Darwin" ]; then
+if [[ ! -f "./BrowserStackLocal" ]]; then
+    if [[ "$(uname)" == "Darwin" ]]; then
         # Do something under Mac OS X platform        
+        apt-get update
+        apt-get install unzip
         wget https://www.browserstack.com/browserstack-local/BrowserStackLocal-darwin-x64.zip
         unzip BrowserStackLocal-darwin-x64.zip
-    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
         # Do something under GNU/Linux platform
         wget http://www.browserstack.com/browserstack-local/BrowserStackLocal-linux-x64.zip
         unzip BrowserStackLocal-linux-x64.zip
