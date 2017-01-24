bc4fb4745f99c4b3cd711bf34eaf2bd946f95888
Avner Sorek <avner.sorek@gmail.com>
2017-01-24 18:45:18 +0200

    adding cluster to all the api's

--- ohe-api/src/index.js
+++ ohe-api/src/index.js
@@ -1,27 +1,16 @@
 'use strict';
-const co = require('co');
 const shared = require('dorbel-shared');
 const config = shared.config; config.setConfigFileFolder(__dirname + '/config'); // load config from file before anything else
 const logger = shared.logger.getLogger(module);
 const db = require('./openHouseEventsDb/dbConnectionProvider');
 const messageBus = shared.utils.messageBus;
 
-logger.info({
-  version: process.env.npm_package_version,
-  env: config.get('NODE_ENV')
-}, 'Starting server');
-
-logger.info({
-  hostname: config.get('RDS_HOSTNAME'),
-  dbname: config.get('RDS_DB_NAME')
-}, 'Connecting to DB');
-
 function startMessageConsumers() {
   const consumer = require('./queueConsumer/messageConsumer');
-  
+
   logger.info('Begin consuming messages from SQS queue');
   const appEventsConsumers = messageBus.consume.start(
-    config.get('OHE_API_EVENTS_SQS_QUEUE_URL'), 
+    config.get('OHE_API_EVENTS_SQS_QUEUE_URL'),
     consumer.handleMessage);
 
   process.on('exit', function (code) {
@@ -36,17 +25,22 @@ function startMessageConsumers() {
 }
 
 function* bootstrap() {
+  logger.info({
+    hostname: config.get('RDS_HOSTNAME'),
+    dbname: config.get('RDS_DB_NAME')
+  }, 'Connecting to DB');
   yield db.connect();
   startMessageConsumers();
   const server = require('./server/server'); // server should be required only after db connect finish
+  logger.info({
+    version: process.env.npm_package_version,
+    env: config.get('NODE_ENV')
+  }, 'Starting server');
   return server.listen();
 }
 
 if (require.main === module) {
-  co(bootstrap).catch(ex => {
-    logger.error('failed to bootstrap application', ex.stack || ex);
-    process.exit(-1);
-  });
+  shared.utils.serverRunner.startCluster(bootstrap);
 }
 
 module.exports = {
