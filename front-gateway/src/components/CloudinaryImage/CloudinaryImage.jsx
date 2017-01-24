2c3f96f3fb0658fb76f17fe82876b0b896699fb0
Idan Langer <lxngxr@users.noreply.github.com>
2017-01-24 10:54:56 +0200

    image resizing (#285)

---
+++ front-gateway/src/components/CloudinaryImage/CloudinaryImage.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class CloudinaryImage extends Component {
+
+  getSrcUrl() {
+    let optionsStr = 'c_fit';
+    if (this.props.height) {
+      optionsStr += ',h_' + this.props.height;
+    }
+    if (this.props.width) {
+      optionsStr += ',w_' + this.props.width;
+    }
+
+    return optionsStr ? this.props.src.replace('upload', 'upload/' + optionsStr) : this.props.src;
+  }
+
+  render() {
+
+
+    return (
+      <img className={this.props.className} src={this.getSrcUrl()} />
+    );
+  }
+
+
+
+}
+
+CloudinaryImage.propTypes = {
+  height: React.PropTypes.number,
+  width: React.PropTypes.number,
+  src: React.PropTypes.string.isRequired,
+  className: React.PropTypes.string
+};
+
+export default CloudinaryImage;
