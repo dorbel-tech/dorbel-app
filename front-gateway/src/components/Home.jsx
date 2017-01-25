2c09db20fb8b865b6081ceba280aa3b0ec7e21e6
David Virtser <david@dorbel.com>
2017-01-25 16:50:01 +0200

    Adding some text

--- front-gateway/src/components/Home.jsx
+++ front-gateway/src/components/Home.jsx
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 class Home extends Component {
   render() {
     return (
-      <div>
+      <div style={{padding: 100}}>
+        <center><h2>Hello</h2></center>
       </div>
     );
   }
