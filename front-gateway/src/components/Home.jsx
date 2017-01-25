d87035fc4b7f5fe8fba2267061bc523e10543e18
David Virtser <david@dorbel.com>
2017-01-25 16:43:30 +0200

    Removed footer in apartment lisitng #276

--- front-gateway/src/components/Home.jsx
+++ front-gateway/src/components/Home.jsx
@@ -3,8 +3,7 @@ import React, { Component } from 'react';
 class Home extends Component {
   render() {
     return (
-      <div style={{padding: 100}}>
-        <center><h2>Hello</h2></center>
+      <div>
       </div>
     );
   }
