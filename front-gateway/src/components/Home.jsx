3eda5b4a021f8cf114f2064a2b2b81811d9afeaa
David Virtser <david@dorbel.com>
2017-01-25 16:25:13 +0200

    Removed profile and homepage links

--- front-gateway/src/components/Home.jsx
+++ front-gateway/src/components/Home.jsx
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
-import NavLink from '~/components/NavLink';
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <ul role="nav">
-          <li><NavLink to="/apartments">Apartments</NavLink></li>
-          <li><NavLink to="/apartments/new_form">Add New Apartment</NavLink></li>
-          <li><NavLink className="login-link" to="/login">Login</NavLink></li>
-          <li><NavLink to="/profile">Profile</NavLink></li>
-        </ul>
+      <div style={{padding: 100}}>
+        <center><h2>Hello</h2></center>
       </div>
     );
   }
--- front-gateway/src/routes.js
+++ front-gateway/src/routes.js
@@ -1,10 +1,8 @@
 'use strict';
-import About from '~/components/About/About';
 import Apartments from '~/components/Apartments/Apartments';
 import Apartment from '~/components/Apartment/Apartment';
 import Home from '~/components/Home';
 import Login from '~/components/Login';
-import Profile from '~/components/Profile';
 import UploadApartmentForm from '~/components/UploadApartmentForm/UploadApartmentForm';
 
 const routes = [
@@ -15,7 +13,6 @@ const routes = [
   { route: '/apartments/:apartmentId/:action/:oheId', view: Apartment },
   { route: '/apartments/:apartmentId/:action', view: Apartment },
   { route: '/apartments/:apartmentId', view: Apartment },
-  { route: '/profile', view: Profile, requireLogin: true }
 ];
 
 module.exports = {
