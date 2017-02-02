6591df18e4e71b2f96c28572c551aad89d894d13
Idan Langer <lxngxr@users.noreply.github.com>
2017-02-02 19:53:35 +0200

    don't wait for OHE to be created (#377)
    * don't wait for OHE to be created
    
    closes #346
    
    * added TODO

--- front-gateway/src/providers/ApartmentsProvider.js
+++ front-gateway/src/providers/ApartmentsProvider.js
@@ -54,7 +54,11 @@ class ApartmentsProvider {
     let createdListing;
     return this.apiProvider.fetch('/api/apartments/v1/listings', { method: 'POST', data: listing })
       .then((newListing) => createdListing = newListing)
-      .then(() => this.oheProvider.createOhe(Object.assign({ listing_id: createdListing.id }, listing.open_house_event)))
+      .then(() => { // TODO: move OHE creation to pub/sub messages on background
+        try {
+          this.oheProvider.createOhe(Object.assign({ listing_id: createdListing.id }, listing.open_house_event));
+        } catch (err) { /*eslint-disable eslint-enable*/ }
+      })
       .then(() => this.appStore.authStore.updateProfile({
         first_name: listing.user.firstname,
         last_name: listing.user.lastname,
