6f091bc4b4a877a84640d9c274e7b4c8308b7912
Avner Sorek <avner.sorek@gmail.com>
2017-02-06 18:22:37 +0200

    testing getOheInfo

--- notifications-service/test/integration/dataRetrieval.spec.js
+++ notifications-service/test/integration/dataRetrieval.spec.js
@@ -3,29 +3,53 @@
 *  This tests the integration between this module and the API's running
 */
 const __ = require('hamjest');
+const moment = require('moment');
 
-// This is bull**** but until we have a CI-environment-seed it's the best I can do
-const staticUser = {
-  id: '96271bc9-6801-4ea5-8680-b3e29eafce0d',
-  email: 'test@test.com'
+// This is meh, but until we have a CI-environment-seed (or something) it's the best I can do
+const fixtures = {
+  staticUser : {
+    id: '96271bc9-6801-4ea5-8680-b3e29eafce0d',
+    email: 'test@test.com'
+  },
+  listing_id: 1,
+  event_id: 1
 };
 
 describe('Data Retrieval Integration', function () {
   before(function* () {
     this.dataRetrieval = require('../../src/sender/dataRetrieval');
+
+    this.retrieve = (funcName, eventData) => {
+      return this.dataRetrieval.getAdditonalData({ dataRetrieval: [ funcName ] }, eventData);
+    };
   });
 
-  it('should get getListingInfo', function* () {
-    const eventConfig = {
-      dataRetrieval: ['getListingInfo']
-    };
-    const eventData = {
-      user_uuid: staticUser.id,
-      listing_id: 1
-    };
-    const results = yield this.dataRetrieval.getAdditonalData(eventConfig, eventData);
+  it('should get-Listing-Info', function* () {
+    const listingInfo = yield this.retrieve('getListingInfo', {
+      user_uuid: fixtures.staticUser.id,
+      listing_id: fixtures.listing_id
+    });
 
-    __.assertThat(results.listing, __.hasProperties({ id: 1, publishing_user_email: staticUser.email }));
+    __.assertThat(listingInfo.listing, __.allOf(
+      __.hasProperties({ id: fixtures.listing_id, publishing_user_email: fixtures.staticUser.email }),
+      __.hasProperty('apartment', __.allOf(
+        __.hasProperty('apt_number'),
+        __.hasProperty('building', __.allOf(
+          __.hasProperty('street_name'),
+          __.hasProperty('house_number'),
+          __.hasProperty('city', __.hasProperty('city_name')),
+        ))
+      ))
+    ));
+  });
+
+  it('should get Ohe Info', function* () {
+    const oheInfo = yield this.retrieve('getOheInfo', {
+      event_id: fixtures.event_id
+    });
+
+    __.assertThat(moment(oheInfo.ohe.start_time, moment.ISO_8601).isValid(), __.is(true));
+    __.assertThat(moment(oheInfo.ohe.end_time, moment.ISO_8601).isValid(), __.is(true));
   });
 
 });
