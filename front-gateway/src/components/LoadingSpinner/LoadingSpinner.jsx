2bf5e4210eeb1efbf5ab142ffd9b086a4823ee1e
Amos Lande <amoslande@users.noreply.github.com>
2017-02-09 15:53:14 +0200

    Fix spinner vertical positioning (#486)
    * Fix spinner vertical positioning
    
    * Fix spinner vertical positioning

--- front-gateway/src/components/LoadingSpinner/LoadingSpinner.jsx
+++ front-gateway/src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import Spinner from 'react-spinkit';
+
 import './LoadingSpinner.scss';
 
 class LoadingSpinner extends Component {
-
   render() {
     return (
-        <Spinner spinnerName="circle" noFadeIn />
+      <Spinner spinnerName="circle" noFadeIn />
     );
   }
 }
