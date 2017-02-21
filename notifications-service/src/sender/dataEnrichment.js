1665daf76506e5c59eb4d0bc128db4b331e693f1
David Virtser <poison77@gmail.com>
2017-02-21 12:08:29 +0200

    Rearrange Intercom for easier operations  (#575)
    * Rearrange Intercom for easier operations #572
    
    * extracted listing url function to dorbel-shared
    
    * upgrade of shared
    
    * _listing_url adding moved to dorbel-shared
    
    * dorbel-shared upgrade

--- notifications-service/src/sender/dataEnrichment.js
+++ notifications-service/src/sender/dataEnrichment.js
@@ -3,10 +3,11 @@
  * To enrich the different data needed by each notification type before it is sent 
  */ 
 'use strict'; 
-const config = require('dorbel-shared').config;
+const shared = require('dorbel-shared');
 const ical = require('ical-generator');
 const tzName = 'Asia/Jerusalem';
-const website_url = config.get('FRONT_GATEWAY_URL') || 'https://app.dorbel.com';
+const generic = shared.utils.generic;
+
 
 // Calendar invite ICS file building. 
 // Requires listing: {...}, ohe: {...} and user_profile: {...} objects in additonalData.
@@ -19,7 +20,7 @@ function buildCalendarInvite(additonalData, method) {
     summary: 'ביקור בדירה ברח׳ ' + additonalData.listing.apartment.building.street_name,
     location: additonalData.listing.apartment.building.street_name + ' ' + additonalData.listing.apartment.building.house_number + ', ' + additonalData.listing.apartment.building.city.city_name,
     description: 'הזמן שלכם ושל בעלי הדירה חשוב לנו, לכן במידה ומשהו משתנה ואינכם יכולים להגיע לביקור, אנא בטלו הגעתכם.',
-    url: website_url + '/apartments/' + additonalData.listing.id,
+    url: generic.getListingUrl(additonalData.listing.id),
     method: method,
     status: method === 'publish' ? 'confirmed' : 'cancelled'
   });
