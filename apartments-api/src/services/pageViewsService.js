bcaf22672274658b6054211dc1cadbb617540a32
Avner Sorek <avner.sorek@gmail.com>
2017-02-28 16:26:08 +0200

    refactoring the mess

--- apartments-api/src/services/pageViewsService.js
+++ apartments-api/src/services/pageViewsService.js
@@ -8,60 +8,78 @@ const logger = shared.logger.getLogger(module);
 const cache = shared.utils.cache;
 
 const PAGE_VIEW_CACHE_TTL_SECONDS = 100;
+const PAGE_VIEW_CACHE_KEY_PREFIX = 'listing_page_views_';
 
 analyticsProvider.init();
 
-function * getPageViewsForListings(listingIds) {
-  logger.trace({ listingIds }, 'getting page views');
-
-  const viewsFromCache = {};
-  const listingsNotInCache = [];
+function * getPageViewsFromCache(listingIds) {
+  const pageViewsFromCache = {};
+  const listingIdsNotInCache = [];
 
   yield listingIds.map(function * (listing_id) {
-    const cachedViews = yield cache.getKey('listing_page_views_' + listing_id);
+    const cachedViews = yield cache.getKey(PAGE_VIEW_CACHE_KEY_PREFIX + listing_id);
     if (cachedViews) {
       logger.trace({ listing_id }, 'page view cache hit');
-      viewsFromCache[listing_id] = JSON.parse(cachedViews);
+      pageViewsFromCache[listing_id] = JSON.parse(cachedViews);
     } else {
       logger.trace({ listing_id }, 'page view cache miss');
-      listingsNotInCache.push(listing_id);
+      listingIdsNotInCache.push(listing_id);
     }
   });
 
-  if (listingsNotInCache.length === 0) {
-    return viewsFromCache;
-  }
+  return { pageViewsFromCache, listingIdsNotInCache };
+}
 
-  const listings = yield listingRepository.getSlugs(listingsNotInCache);
+function * getPageViewsFromProvider(listingsIds) {
+  const listings = yield listingRepository.getSlugs(listingsIds);
 
-  const urls = [];
-  const idsByUrl = {};
-  listings.forEach(listing => {
-    const idUrl = `/apartments/${listing.id}`;
-    urls.push(idUrl);
-    idsByUrl[idUrl] = listing.id;
+  const idsByUrl = getIdsByUrl(listings);
+  const urls = Object.keys(idsByUrl);
 
-    if (listing.slug) {
-      const slugUrl = `/apartments/${listing.slug}`;
-      urls.push(slugUrl);
-      idsByUrl[slugUrl] = listing.id;
-    }
-  });
+  const pageViews = yield analyticsProvider.getPageViews(urls);
 
-  const pageViewsByUrl = yield analyticsProvider.getPageViews(urls);
   const viewsFromProvider = {};
-
-  pageViewsByUrl.forEach(result => {
+  pageViews.forEach(result => {
     const listingId = idsByUrl[result.url];
     viewsFromProvider[listingId] = viewsFromProvider[listingId] || { views: 0 };
     viewsFromProvider[listingId].views += result.views;
   });
 
-  _.each(viewsFromProvider, (views, id) => {
-    cache.setKey('listing_page_views_' + id, JSON.stringify(views), PAGE_VIEW_CACHE_TTL_SECONDS);
-  });
+  return viewsFromProvider;
+}
 
-  return _.merge(viewsFromProvider, viewsFromCache);
+function getIdsByUrl(listings) {
+  // return a map between URL => listing_id (listings with slug will have two entries)
+  const idsByUrl = {};
+  listings.forEach(listing => {
+    idsByUrl[`/apartments/${listing.id}`] = listing.id;
+    if (listing.slug) {
+      idsByUrl[`/apartments/${listing.slug}`] = listing.id;
+    }
+  });
+  return idsByUrl;
+}
+
+function setPageViewsInCache(viewsFromProvider) {
+  _.each(viewsFromProvider, (views, id) => {
+    cache.setKey(PAGE_VIEW_CACHE_KEY_PREFIX + id, JSON.stringify(views), PAGE_VIEW_CACHE_TTL_SECONDS);
+  });
+}
+
+function * getPageViewsForListings(listingIds) {
+  logger.trace({ listingIds }, 'getting page views');
+
+  const cacheResults = yield getPageViewsFromCache(listingIds);
+
+  if (cacheResults.listingIdsNotInCache.length === 0) { // if we got all the listings
+    return cacheResults.pageViewsFromCache;
+  }
+
+  const viewsFromProvider = yield getPageViewsFromProvider(cacheResults.listingIdsNotInCache);
+
+  setPageViewsInCache(viewsFromProvider);
+
+  return _.merge(cacheResults.pageViewsFromCache, viewsFromProvider);
 }
 
 module.exports = {
