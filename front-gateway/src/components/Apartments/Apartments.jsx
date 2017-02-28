4d0c628a80955c1921a060ce5458e186d55fd4a5
Amos Lande <amoslande@users.noreply.github.com>
2017-02-28 11:59:10 +0200

    Remove white background at the footer of search page (few listings) (#640)

--- front-gateway/src/components/Apartments/Apartments.jsx
+++ front-gateway/src/components/Apartments/Apartments.jsx
@@ -233,7 +233,7 @@ class Apartments extends Component {
       cityTitle = city ? city.city_name : 'טוען...';
     }
 
-    return <div>
+    return <div className="apartments-container">
       <div className="apartments-filter-toggle-container">
         <Button onClick={this.toggleHideFilter}>
           סנן תוצאות
@@ -371,7 +371,7 @@ class Apartments extends Component {
 
     if (!this.state.isLoading && results.length > 0) {
       return (<Grid fluid>
-        <Row className="apartments-results-container">
+        <Row>
           {results.map(listing => <ListingThumbnail listing={listing} key={listing.id} />)}
         </Row>
       </Grid>);
@@ -391,7 +391,7 @@ class Apartments extends Component {
 
   render() {
     return (
-      <div className="apartments-container">
+      <div className="apartments-wrapper">
         {this.renderFilter()}
         <div className="apartments-results-wrapper">
           {this.renderResults()}
