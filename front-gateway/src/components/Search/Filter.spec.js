5d56dedc59ae6cf6d8cf2618e2d5df11bc28fa7b
Amos Lande <amos@dorbel.com>
2017-03-05 12:44:22 +0200

    Filter component unit tests

---
+++ front-gateway/src/components/Search/Filter.spec.js
@@ -0,0 +1,58 @@
+'use strict';
+import { mount } from 'enzyme';
+import React from 'react';
+
+import Filter from './Filter';
+
+describe('Filter', () => {
+  let props;
+  let mountedFilter;
+
+  const filter = () => {
+    if (!mountedFilter) {
+      mountedFilter = mount(
+        <Filter {...props} />
+      );
+    }
+    return mountedFilter;
+  }
+
+  beforeEach(() => {
+    spyOn(history, 'pushState');
+
+    props = {
+      appStore: {
+        authStore: {},
+        cityStore: {
+          cities: []
+        }
+      },
+      appProviders: {
+        cityProvider: {loadCities: jest.fn()},
+        searchProvider: {
+          initFilter: jest.fn(),
+          search: jest.fn()
+        }
+      }
+    };
+    mountedFilter = undefined;
+  });
+
+  describe('Initialization', () => {
+    it('should initialize the component', () => {
+      filter();
+
+      expect(props.appProviders.searchProvider.initFilter).toHaveBeenCalledWith();
+      expect(props.appProviders.cityProvider.loadCities).toHaveBeenCalledWith();
+      expect(props.appProviders.searchProvider.search).toHaveBeenCalledWith({"city": 1});
+      expect(mountedFilter.state()).toBeNull();
+    });
+
+    it('should initialize the component with state from location', () => {
+      //spyOn(JSON, 'parse').and.returnValue({room: true});
+      filter();
+
+      expect(mountedFilter.state()).toBeNull();
+    });
+  });
+});
