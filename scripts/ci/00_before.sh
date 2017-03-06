37b4d67eb0d656d8e04b37ab30f7569c1b43a564
David Virtser <poison77@gmail.com>
2017-03-06 14:52:16 +0200

    Improving CI commands to be part of repository (#683)
    * imroving ci commands to be part of repository
    
    * testing new feature

---
+++ scripts/ci/00_before.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Cleanup when going out of space
+find ~/.rbenv/versions -maxdepth 1 -type d | grep -v "$(rbenv version | awk '{print $1}')" | tail -n +2 | xargs rm -rf
+find ~/.nvm/versions/node/ -maxdepth 1 -type d | grep -v "$(node --version)" | tail -n +2 | xargs rm -rf
+rm -rf ~/.phpbrew/
+
+# Cache restore, disabled as inconsitent.
+# docker-cache restore
+
+# Creating directories for codecov.io for code coverage report mounted volume.
+mkdir apartments-api-shared
+mkdir ohe-api-shared
+mkdir notifications-service-shared
+mkdir front-gateway-shared
+
+# Checking wich directories were changed in this branch to execute only the relevant build/test/deploy.
+changed_directories=$(git diff-tree --no-commit-id --name-only $REVISION); echo $changed_directories
+
+# Starting docker composition
+docker-compose -f docker-compose.yml -f docker-compose.ci.yml up -d --force-recreate --build
---
+++ scripts/ci/99_after.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Remove container to save space.
+# docker rmi mysql
+
+# Take docker images cache snapshot, disabled as inconcictent.
+# docker-cache snapshot
