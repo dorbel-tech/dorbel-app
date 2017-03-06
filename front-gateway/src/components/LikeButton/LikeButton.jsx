dcb6291c0a3ab82a880a6b02796ed69e1ed80de7
Idan Langer <lxngxr@gmail.com>
2017-03-06 18:26:37 +0200

    /listinglike(client): set like - mid work

--- front-gateway/src/components/LikeButton/LikeButton.jsx
+++ front-gateway/src/components/LikeButton/LikeButton.jsx
@@ -5,8 +5,8 @@ import './LikeButton.scss';
 
 @observer(['appProviders', 'appStore'])
 class LikeButton extends Component {
-
-  handleClick() {
+  handleClick(e) {
+    e.preventDefault();
     if (this.props.appStore.authStore.isLoggedIn) {
       let prevState = this.props.appProviders.likeProvider.get(this.props.listingId);
       this.props.appProviders.likeProvider.set(this.props.listingId, !prevState);
