e34e735c4fc504774a85521d51f179283b81d370
David Virtser <david@dorbel.com>
2017-03-09 11:06:55 +0200

    revert e2e loop change

--- scripts/ci/e2e_run.sh
+++ scripts/ci/e2e_run.sh
@@ -2,12 +2,4 @@
 set -x # Print all commands to terminal as executed.
 
 cd front-gateway && yarn install
-
-# Run the command until get 0 exit code or up to 3 times to fight flakiness.
-try=1
-FRONT_GATEWAY_URL=$1 yarn run test:e2e:browserstack 
-while [ $? -ne 0 ] && [ $try -lt 3 ]; do 
-  ((try++))
-  FRONT_GATEWAY_URL=$1 yarn run test:e2e:browserstack 
-done
-exit $?
+FRONT_GATEWAY_URL=$1 yarn run test:e2e:browserstack
