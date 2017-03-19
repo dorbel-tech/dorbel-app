4cb6fd92f5aead37fbf994d7551def66c5218b63
Amos Lande <amos@dorbel.com>
2017-03-19 17:22:14 +0200

    Listing page design Feb 2017

--- front-gateway/test/e2e/suites/listing_actions.js
+++ front-gateway/test/e2e/suites/listing_actions.js
@@ -32,7 +32,7 @@ function waitForUnRegisterText() {
 }
 
 function waitForRegisterText() {
-  common.waitForText(listing.section.oheList, '@firstEventText', 'נרשמתם לארוע זה. לחצו לביטול');
+  common.waitForText(listing.section.oheList, '@firstEventText', 'לחצו לביטול הרשמה למועד זה');
 }
 
 function waitForUnFollowText() {
