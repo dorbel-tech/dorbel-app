d469a2c24cd932fabbbff0f0ff0a1a9973ee1e3d
Amos Lande <amos@dorbel.com>
2017-03-21 18:18:18 +0200

    Listing page design Feb 2017

--- front-gateway/src/components/Listing/components/ListingDescription.jsx
+++ front-gateway/src/components/Listing/components/ListingDescription.jsx
@@ -18,14 +18,14 @@ class ListingDescription extends React.Component {
 
   render() {
     const { listing } = this.props;
-    const listingTax = listing.property_tax ? <p>ארנונה: <span>{listing.property_tax}</span></p> : null;
-    const listingFee = listing.board_fee ? <p>ועד בית: <span>{listing.board_fee}</span></p> : null;
+    const listingTax = <p> ארנונה: {listing.property_tax ? '<span>' + listing.property_tax + '</span>' : '--'}</p>;
+    const listingFee = <p>ועד בית: {listing.board_fee ? '<span>' + listing.board_fee + '</span>' : '--'}</p>;
     const listingPrices = <div><p>שכר דירה: <span>{listing.monthly_rent}</span></p>{listingTax}{listingFee}</div>;
 
     return (
       <Row className="listing-description-container">
         {this.renderDescriptionRow('תאריך כניסה', <p>{this.props.appProviders.utils.formatDate(listing.lease_start)}</p>)}
-        {listing.description ? this.renderDescriptionRow('תאור הנכס', <p>{listing.description}</p>) : null}
+        {this.renderDescriptionRow('תאור הנכס', <p>{listing.description || '(אין תאור)'}</p>)}
         <Row className="listing-description-item">
           <Col md={2}>
             <h5>פרטי הנכס</h5>
