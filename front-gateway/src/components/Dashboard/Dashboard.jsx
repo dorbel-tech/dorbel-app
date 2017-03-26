3267cfe2e3fd4cb6efe064ac2c2eecfcb1b01c8a
Amos Lande <amos@dorbel.com>
2017-03-26 14:34:20 +0300

    Dashboard menu logout action

--- front-gateway/src/components/Dashboard/Dashboard.jsx
+++ front-gateway/src/components/Dashboard/Dashboard.jsx
@@ -7,11 +7,18 @@ import NavLink from '~/components/NavLink';
 
 import './Dashboard.scss';
 
-@observer(['appStore', 'appProviders'])
+@observer(['appStore', 'appProviders', 'router'])
 class Dashboard extends Component {
   constructor(props) {
     super(props);
     autobind(this);
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (newProps.action === 'logout') {
+      newProps.appProviders.authProvider.logout();
+      newProps.router.setRoute('/');
+    }
   }
 
   renderAction() {
@@ -19,11 +26,19 @@ class Dashboard extends Component {
     return <div>{this.props.action}</div>
   }
 
+  renderMenuItem(faIconClassName, menuText, navTo) {
+    return <div>
+        <i className={'fa ' + faIconClassName}  aria-hidden="true"></i>
+        <NavLink to={navTo}>{menuText}</NavLink>
+      </div>
+  }
+
   render() {
     return <div className="dashboard-container" onScroll={this.handleScroll}>
         <div className="dashboard-menu-wrapper">
-          <div><NavLink to="/dashboard/listings">הנכסים שלי</NavLink></div>
-          <div><NavLink to="/dashboard/likes">המועדפים שלי</NavLink></div>
+          {this.renderMenuItem('fa-home', 'הנכסים שלי', '/dashboard/listings')}
+          {this.renderMenuItem('fa-heart', 'המועדפים שלי', '/dashboard/likes')}
+          {this.renderMenuItem('fa-sign-out', 'יציאה', '/dashboard/logout')}
         </div>
         <div className="dashboard-action-wrapper">
           {this.renderAction()}
@@ -35,7 +50,8 @@ class Dashboard extends Component {
 Dashboard.wrappedComponent.propTypes = {
   action: React.PropTypes.string,
   appStore: React.PropTypes.object.isRequired,
-  appProviders: React.PropTypes.object.isRequired
+  appProviders: React.PropTypes.object.isRequired,
+  router: React.PropTypes.any
 };
 
 export default Dashboard;
