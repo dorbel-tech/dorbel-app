1250683b73b4a921d5f54d0d3db96df5eb9d97ad
David Virtser <david@dorbel.com>
2017-03-26 18:17:13 +0300

    bug fix

--- front-gateway/src/server/buildOutputs.js
+++ front-gateway/src/server/buildOutputs.js
@@ -10,9 +10,8 @@ function setBuildFiles(jsBundle, cssBundle) {
 
 function getBuildOutputs(app) {
   // Used for development only
-  const hotReloadPort = process.env.HOT_RELOAD_SERVER_PORT || 8888;
-  if (hotReloadPort) {
-    const buildHost = 'http://localhost:' + hotReloadPort;
+  if (process.env.HOT_RELOAD_SERVER_PORT) {
+    const buildHost = 'http://localhost:' + process.env.HOT_RELOAD_SERVER_PORT;
 
     app.use(setBuildFiles(
       `${buildHost}/build/bundle.js`,
