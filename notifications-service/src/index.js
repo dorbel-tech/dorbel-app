97bf69b5c733ea2a8d3dcb46eb1d93ed79312f9c
David Virtser <david@dorbel.com>
2017-03-26 17:09:13 +0300

    using dotenv instead of custom config in ohe-api

--- notifications-service/src/index.js
+++ notifications-service/src/index.js
@@ -1,20 +1,19 @@
 'use strict';
+require('dotenv')
 const shared = require('dorbel-shared');
-const config = shared.config;
-const path = require('path'); config.setConfigFileFolder(path.join(__dirname, '/config')); // load config from file before anything else
 const logger = shared.logger.getLogger(module);
 const notificationSender = require('./sender/notificationSender');
 const messageBus = shared.utils.messageBus;
 
 logger.info({
   version: process.env.npm_package_version,
-  env: config.get('NODE_ENV')
+  env: process.env.NODE_ENV
 }, 'Starting server');
 
 function startMessageConsumers() {
   logger.info('Begin consuming messages from SQS queue');
   const appEventsConsumers = messageBus.consume.start(
-    config.get('NOTIFICATIONS_APP_EVENTS_SQS_QUEUE_URL'),
+    process.env.NOTIFICATIONS_APP_EVENTS_SQS_QUEUE_URL,
     notificationSender.handleMessage);
 
   process.on('exit', function (code) {
