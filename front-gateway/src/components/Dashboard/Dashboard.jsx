6d6acf0a986ffe54f88a89515b5b2f5647a71d6a
Amos Lande <amos@dorbel.com>
2017-03-26 18:46:13 +0300

    Dashboard menu initial style

--- front-gateway/src/components/Dashboard/Dashboard.jsx
+++ front-gateway/src/components/Dashboard/Dashboard.jsx
@@ -27,18 +27,27 @@ class Dashboard extends Component {
   }
 
   renderMenuItem(faIconClassName, menuText, navTo) {
-    return <div>
-        <i className={'fa ' + faIconClassName}  aria-hidden="true"></i>
-        <NavLink to={navTo}>{menuText}</NavLink>
+    const isSelected = this.props.action === navTo;
+
+    return <div className={'dashboard-menu-item ' + (isSelected ? 'dashboard-menu-item-selected' : '')}>
+        <i className={'dashboard-menu-item-icon fa ' + faIconClassName}  aria-hidden="true"></i>
+        <NavLink to={'/dashboard/' + navTo}>{menuText}</NavLink>
       </div>
   }
 
   render() {
+    const profile = this.props.appStore.authStore.profile || {};
+    const firstName = profile.first_name || '';
+
     return <div className="dashboard-container" onScroll={this.handleScroll}>
         <div className="dashboard-menu-wrapper">
-          {this.renderMenuItem('fa-home', 'הנכסים שלי', '/dashboard/listings')}
-          {this.renderMenuItem('fa-heart', 'המועדפים שלי', '/dashboard/likes')}
-          {this.renderMenuItem('fa-sign-out', 'יציאה', '/dashboard/logout')}
+          <div className="dashboard-menu-profile-section">
+            <img src={profile.picture} className="dashboard-menu-profile-image" />
+            <div className="dashboard-menu-profile-text">{firstName}</div>
+          </div>
+          {this.renderMenuItem('fa-home', 'הנכסים שלי', 'listings')}
+          {this.renderMenuItem('fa-heart', 'המועדפים שלי', 'likes')}
+          {this.renderMenuItem('fa-sign-out', 'יציאה', 'logout')}
         </div>
         <div className="dashboard-action-wrapper">
           {this.renderAction()}
