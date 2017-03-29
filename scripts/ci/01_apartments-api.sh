7a3bdf78b8a4facce44236f67842acf457d2283e
David Virtser <david@dorbel.com>
2017-03-29 18:08:31 +0300

    trying to fix CI

--- scripts/ci/01_apartments-api.sh
+++ scripts/ci/01_apartments-api.sh
@@ -3,7 +3,7 @@ set -x # Print all commands to terminal as executed.
 set -e # Any subsequent(*) commands which fail will cause the shell script to exit immediately.
 
 docker-compose exec apartments-api yarn run lint
-docker-compose exec apartments-api bash -c "./scripts/wait-for-it.sh db:3306 --timeout=180 && yarn run db:migrate; yarn run db:seed && yarn run test:seed"
+# docker-compose exec apartments-api bash -c "./scripts/wait-for-it.sh db:3306 --timeout=180 && yarn run db:migrate; yarn run db:seed && yarn run test:seed"
 docker-compose exec apartments-api yarn run test
 docker-compose exec apartments-api cp -v ./test/coverage/coverage-final.json /shared/
 docker-compose exec apartments-api yarn run test:integration
