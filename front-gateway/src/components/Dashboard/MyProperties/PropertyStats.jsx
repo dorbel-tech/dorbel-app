fa48bb2e6f3db42c8b1719e6908baa80c99fa950
David Virtser <david@dorbel.com>
2017-04-05 12:16:18 +0300

    PR fixes

--- front-gateway/src/components/Dashboard/MyProperties/PropertyStats.jsx
+++ front-gateway/src/components/Dashboard/MyProperties/PropertyStats.jsx
@@ -48,91 +48,97 @@ class PropertyStats extends Component {
     const daysPassed = moment(Date.now()).diff(moment(listing.created_at), 'days');
     const listingRented = listing.status === 'rented' || listing.status === 'unlisted';
 
-    return  <Grid fluid={true} className="dashboard-my-propery-stats">
-                <Row className="rent-title">
+    return  <Grid fluid className="propery-stats">
+                <Row className="propery-stats-rent-title">
                   <Col xs={12}>
                     תהליך ההשכרה:
                   </Col>
                 </Row>
                 <Row>
                   <Col xs={12}>
-                    <div className="numbers-row">
-                      <div className={'number' + (views > 0 ? ' active-color': '')}>{views}</div>
-                      <div className="empty"></div>
-                      <div className={'number' + (registrations > 0 ? ' active-color': '')}>{registrations}</div>
-                      <div className="empty"></div>
-                      <div className={'number rented-check' + (listingRented ? ' active-color': '')}>
+                    <div className="propery-stats-numbers-row">
+                      <div className={'propery-stats-number' + (views > 0 ? ' propery-stats-number-not-empty': '')}>{views}</div>
+                      <div className="propery-stats-empty"></div>
+                      <div className={'propery-stats-number' + (registrations > 0 ? ' propery-stats-number-not-empty': '')}>{registrations}</div>
+                      <div className="propery-stats-empty"></div>
+                      <div className={'propery-stats-number propery-stats-rented-check' + (listingRented ? ' propery-stats-number-not-empty': '')}>
                         <i className="fa fa-check" aria-hidden="true"></i>
                       </div>
                     </div>
                     <div>
-                      <div className={'bubble' + (views > 0 ? ' active-color-border': '')}>
-                        <div className="bubble-text">צפיות במודעה</div>
+                      <div className={'propery-stats-bubble' + (views > 0 ? ' propery-stats-bubble-not-empty': '')}>
+                        <div className="propery-stats-bubble-text">צפיות במודעה</div>
                       </div>
-                      <div className={'line' + (registrations > 0 ? ' active-color-border-bottom': '')}></div>
-                      <div className={'bubble' + (registrations > 0 ? ' active-color-border': '')}>
-                        <div className="bubble-text">הרשמות לביקורים</div>
+                      <div className={'propery-stats-line' + (registrations > 0 ? ' propery-stats-line-not-empty': '')}></div>
+                      <div className={'propery-stats-bubble' + (registrations > 0 ? ' propery-stats-bubble-not-empty': '')}>
+                        <div className="propery-stats-bubble-text">הרשמות לביקורים</div>
                       </div>
-                      <div className={'line' + (listingRented ? ' active-color-border-bottom': '')}></div>
-                      <div className={'bubble' + (listingRented ? ' active-color-border': '')}>
-                        <div className="bubble-text">חתימת חוזה</div>
+                      <div className={'propery-stats-line' + (listingRented ? ' propery-stats-line-not-empty': '')}></div>
+                      <div className={'propery-stats-bubble' + (listingRented ? ' propery-stats-bubble-not-empty': '')}>
+                        <div className="propery-stats-bubble-text">חתימת חוזה</div>
                       </div>
                     </div>
                   </Col>
                 </Row>
-                <Row className="publishing-title">
+                <Row className="propery-stats-publishing-title">
                   <Col xs={12}>
                     תאריך פרסום המודעה: {listingCreatedAt || null}
                   </Col>
                 </Row>
-                <Row className="listing-stats">
+                <Row className="propery-stats-listing-stats">
                   <Col xs={4}>
-                    <div className="card">
-                      <div className="number">{followers}</div>
-                      <div className="title">עוקבים</div>
+                    <div className="propery-stats-card">
+                      <div className="propery-stats-number">{followers}</div>
+                      <div className="propery-stats-title">עוקבים</div>
                     </div>
                   </Col>
                   <Col xs={4}>
-                    <div className="card">
-                      <div className="number">{listing.totalLikes}</div>
-                      <div className="title">לייקים</div>
+                    <div className="propery-stats-card">
+                      <div className="propery-stats-number">{listing.totalLikes}</div>
+                      <div className="propery-stats-title">לייקים</div>
                     </div>
                   </Col>
                   <Col xs={4}>
-                    <div className="card">
-                      <div className="number">{daysPassed}</div>
-                      <div className="title">ימים שחלפו</div>
+                    <div className="propery-stats-card">
+                      <div className="propery-stats-number">{daysPassed}</div>
+                      <div className="propery-stats-title">ימים שחלפו</div>
                     </div>
                   </Col>
                 </Row>
-                <Row className="services-title">
+                <Row className="propery-stats-services-title">
                   <Col xs={12}>
                     שירותים בתשלום:
                   </Col>
                 </Row>
-                <Row className="services">
+                <Row className="propery-stats-services">
                   <Col md={4}>
-                    <div className="service">
-                      <img className="image" src="https://s3.eu-central-1.amazonaws.com/dorbel-site-assets/images/dashboard/service-publishing-icon.svg"/>
-                      <div className="title">פרסום הדירה</div>
+                    <div className="propery-stats-service">
+                      <img className="propery-stats-image" 
+                          src="https://s3.eu-central-1.amazonaws.com/dorbel-site-assets/images/dashboard/service-publishing-icon.svg"/>
+                      <div className="propery-stats-title">פרסום הדירה</div>
                       <div>רוצים להשכיר מהר? הגדילו את חשיפת הדירה ע״י פרסום ממומן. הגיעו ליותר דיירים בפחות זמן</div>
-                      <a className="button btn btn-success" href="https://www.dorbel.com/pages/services/social-advertising" target="_blank">לפרטים והזמנה</a>
+                      <a className="propery-stats-button btn btn-success" 
+                          href="https://www.dorbel.com/pages/services/social-advertising" target="_blank">לפרטים והזמנה</a>
                     </div>
                   </Col>
                   <Col md={4}>
-                    <div className="service">
-                      <img className="image" src="https://s3.eu-central-1.amazonaws.com/dorbel-site-assets/images/dashboard/service-credit-score-icon.svg"/>
-                      <div className="title">דו״ח נתוני אשראי</div>
+                    <div className="propery-stats-service">
+                      <img className="propery-stats-image" 
+                          src="https://s3.eu-central-1.amazonaws.com/dorbel-site-assets/images/dashboard/service-credit-score-icon.svg"/>
+                      <div className="propery-stats-title">דו״ח נתוני אשראי</div>
                       <div>רוצים להיות בטוחים? לפני חתימה על חוזה חשוב לוודא שהדייר החדש והערבים יעמדו בתשלומים</div>
-                      <a className="button btn btn-success" href="https://www.dorbel.com/pages/services/credit-report" target="_blank">לפרטים והזמנה</a>
+                      <a className="propery-stats-button btn btn-success" 
+                          href="https://www.dorbel.com/pages/services/credit-report" target="_blank">לפרטים והזמנה</a>
                     </div>
                   </Col>
                   <Col md={4}>
-                    <div className="service">
-                      <img className="image" src="https://s3.eu-central-1.amazonaws.com/dorbel-site-assets/images/dashboard/service-photography-icon.svg"/>
-                      <div className="title">צילום והצגת הדירה</div>
+                    <div className="propery-stats-service">
+                      <img className="propery-stats-image" 
+                          src="https://s3.eu-central-1.amazonaws.com/dorbel-site-assets/images/dashboard/service-photography-icon.svg"/>
+                      <div className="propery-stats-title">צילום והצגת הדירה</div>
                       <div>אנו נצלם ונציג עבורכם את הדירה בפני הדיירים הפוטנציאליים כדי לחסוך לכם את ההגעה לכל ביקור</div>
-                      <a className="button btn btn-success" href="https://www.dorbel.com/pages/services/hosted-viewings" target="_blank">לפרטים והזמנה</a>
+                      <a className="propery-stats-button btn btn-success" 
+                          href="https://www.dorbel.com/pages/services/hosted-viewings" target="_blank">לפרטים והזמנה</a>
                     </div>
                   </Col>
                 </Row>
