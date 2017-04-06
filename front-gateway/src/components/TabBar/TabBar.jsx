d5b7e8500344d98e8bdb9f962b39cd039c05fd20
Idan Langer <lxngxr@gmail.com>
2017-04-06 18:14:30 +0300

    Create reusable tabs component

---
+++ front-gateway/src/components/TabBar/TabBar.jsx
@@ -0,0 +1,34 @@
+'use strict';
+import React from 'react';
+import { observer } from 'mobx-react';
+import { Nav, NavItem, Navbar } from 'react-bootstrap';
+
+import './TabBar.scss';
+
+@observer(['router'])
+export default class ListingMenu extends React.Component {
+
+  onClick(activeTab) {
+    this.setState({activeTabName: activeTab.name});
+    this.props.onClick(activeTab);
+  }
+
+  render() {
+    return (
+      <Navbar className="listing-menu-tabs">
+        <Nav bsStyle="tabs" onSelect={this.changeTab}>
+          {this.props.tabs.map(tab =>
+            <NavItem className={tab.isActive ? 'active' : ''} key={tab.title} onClick={() => { this.onClick(tab); }}>
+              {tab.title}
+            </NavItem>
+          )}
+        </Nav>
+      </Navbar>
+    );
+  }
+}
+
+ListingMenu.wrappedComponent.propTypes = {
+  tabs: React.PropTypes.array.isRequired,
+  onClick: React.PropTypes.func
+};
