b164bf8862b43c63c070d81fa5beb950bb403327
Idan Langer <lxngxr@gmail.com>
2017-04-12 15:06:09 +0300

    field validation

--- apartments-api/src/services/userProfileService.js
+++ apartments-api/src/services/userProfileService.js
@@ -5,32 +5,32 @@ const _ = require('lodash');
 const errors = shared.utils.domainErrors;
 const userManagement = shared.utils.userManagement;
 
-const whitelistedProfileParams = {
-  main: [
-    'first_name',
-    'last_name',
-    'phone',
-    'email'
-  ],
-  tenant_profile: [
-    'about_you',
-    'work_place',
-    'position',
-    'facebook_url',
-    'linkedin_url'
-  ]
+const ProfileSectionParams = {
+  main: {
+    first_name: { isRequired: true },
+    last_name: { isRequired: true },
+    phone: { isRequired: true },
+    email: { isRequired: true }
+  },
+  tenant_profile: {
+    about_you: { isRequired: false },
+    work_place: { isRequired: false },
+    position: { isRequired: false },
+    facebook_url: { isRequired: false },
+    linkedin_url: { isRequired: false }
+  }
 };
 
 function* update(user, profileData) {
   if (user) {
-    const profileSection = profileData.profile_section;
+    const profileSectionName = profileData.profile_section;
     delete profileData.profile_section;
-    
-    throwErrorOnJunkParams(profileSection, profileData);
+
+    validateProfileData(profileSectionName, profileData);
 
     logger.info({ user_uuid: user.id, userData: profileData }, 'Updating user details');
     const newUserProfile = yield userManagement.updateUserDetails(user.id, {
-      user_metadata: (profileSection == 'main') ? profileData : { [profileSection]: profileData }
+      user_metadata: (profileSectionName == 'main') ? profileData : { [profileSectionName]: profileData }
     });
     logger.info({ user_uuid: user.id, userData: newUserProfile }, 'Updated user details');
 
@@ -42,12 +42,17 @@ function* update(user, profileData) {
   }
 }
 
-function throwErrorOnJunkParams(profileSection, profileData) { // Allow only whitelisted fields
+function validateProfileData(profileSectionName, profileData) { // Allow only whitelisted fields
+  const fieldMap = ProfileSectionParams[profileSectionName];
   const keysToUpdate = _.keys(profileData);
-  const allowedKeys = whitelistedProfileParams[profileSection] || [];
-  keysToUpdate.forEach((key) => {
-    if (allowedKeys.indexOf(key) == -1) {
-      throw new errors.DomainValidationError('FieldNotAllowed', { field: key }, 'The update request contains an illegal, not white listed, field!');
+  const sectionFieldKeys = _.keys(fieldMap) || [];
+
+  sectionFieldKeys.forEach((key) => {
+    if (keysToUpdate.indexOf(key) == -1) {
+      throw new errors.DomainValidationError('FieldNotAllowed', { field: key }, 'The update request contains an illegal, not white listed field!');
+    }
+    else if (fieldMap[key].isRequired && (profileData[key] == '' || profileData[key] == undefined)) {
+      throw new errors.DomainValidationError('RequiredFieldMissing', { field: key }, `The update request doesn't contain a value for the '${key}' required field`);
     }
   });
 }
