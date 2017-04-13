6f88837f0ca1f41f2078092cffd566c29b37d029
Amos Lande <amoslande@users.noreply.github.com>
2017-04-13 17:11:05 +0300

    Feature/dashboard property header style fix (#847)
    * Dashboard property header style fixes
    
    * Dashboard property header style fixes
    
    * Dashboard property header style fixes
    
    * Removed showDashboardMenu feature toggle
    
    * Dashboard property header style fixes

--- front-gateway/test/e2e/pages/property.js
+++ front-gateway/test/e2e/pages/property.js
@@ -10,7 +10,7 @@ module.exports = {
       selector: '.property-actions-wrapper',
       elements: {
         propertyRefresh: {
-          selector: '.property-refresh-button'
+          selector: '.fa-refresh.property-action-button'
         }
       }
     },
