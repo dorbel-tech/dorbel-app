2ae7ede49174fcaf576dc5642deb431b0218d6a9
David Virtser <david@dorbel.com>
2017-04-13 21:24:52 +0300

    add return

--- ohe-api/src/openHouseEventsDb/migrations/20170413121809-migration-of-ohe-is_active-to-status.js
+++ ohe-api/src/openHouseEventsDb/migrations/20170413121809-migration-of-ohe-is_active-to-status.js
@@ -3,7 +3,7 @@ const logger = require('dorbel-shared').logger.getLogger(module);
 
 module.exports = {
   up: function (queryInterface) {
-    queryInterface.sequelize.query(
+    return queryInterface.sequelize.query(
       'UPDATE open_house_events.open_house_events set status = "deleted" WHERE is_active = 0'
     )
     .then((updateRes) => { logger.info({ res: updateRes }, 'Succesfully migrated OHEs with new status'); })
