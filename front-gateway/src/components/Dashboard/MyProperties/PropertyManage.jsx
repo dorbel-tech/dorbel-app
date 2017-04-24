c9ae586ed7eb9aec2e93ce20605c2f0bb40ebc47
Amos Lande <amos@dorbel.com>
2017-04-24 16:24:57 +0300

    Dashboard property manage tab lease period edit initial commit

--- front-gateway/src/components/Dashboard/MyProperties/PropertyManage.jsx
+++ front-gateway/src/components/Dashboard/MyProperties/PropertyManage.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
 import { ProgressBar, Col, Grid, Row } from 'react-bootstrap';
+import autobind from 'react-autobind';
+import DatePicker from '~/components/DatePicker/DatePicker';
 import utils from '~/providers/utils';
 import moment from 'moment';
 
@@ -8,13 +10,47 @@ import './PropertyManage.scss';
 
 @inject('appProviders') @observer
 class PropertyManage extends Component {
+  constructor(props) {
+    super(props);
+    autobind(this);
+  }
+
+  editLeaseDates() {
+    const { listing } = this.props;
+    const { modalProvider } = this.props.appProviders;
+
+    const modalBody = <div className="property-manage-modal-body">
+        <div>
+          <span className="pull-right">תחילת השכירות</span>
+          <span className="pull-left">תום השכירות</span>
+        </div>
+        <div className="property-manage-modal-picker-container">
+          <DatePicker value={listing.lease_start} />
+          <span>-</span>
+          <DatePicker value={listing.lease_end} />
+        </div>
+      </div>;
+
+    modalProvider.showConfirmationModal({
+      title: 'עדכון תקופת שכירות',
+      heading: 'עדכנו את מועדי תחילת ותום השכירות',
+      body: modalBody,
+      confirmButton: 'עדכן פרטים',
+      confirmStyle: 'success'
+    });
+  }
+
   render() {
     const { listing } = this.props;
     const listingLeaseStart = utils.formatDate(listing.lease_start);
     const listingLeaseEnd = utils.formatDate(listing.lease_end);
     const listingSet = listing.lease_end && listing.lease_start;
-    const daysPassed = listingSet ? moment(listing.lease_start).diff(moment(listing.lease_end), 'days') : false;
-    const daysPassedLabel = daysPassed || '---';
+    const leasePeriod = listingSet ? moment(listing.lease_start).diff(moment(listing.lease_end), 'days') : false;
+    const daysPassed = listingSet ? moment(listing.lease_start).diff(moment(), 'days') : false;
+    const daysLeft = listingSet ? leasePeriod - daysPassed : false;
+    const leasePeriodLabel = leasePeriod || '-';
+    const daysPassedLabel = daysPassed || '-';
+    const daysLeftLabel = daysLeft || '-';
 
     return  <Grid fluid className="property-manage">
               <Row className="property-manage-lease-title">
@@ -25,9 +61,9 @@ class PropertyManage extends Component {
               <Row className="property-manage-lease-period">
                 <Col xs={12}>
                   <div>
-                    משך תקופת שכירות נוכחית: {daysPassedLabel} ימים
+                    משך תקופת שכירות נוכחית: {leasePeriodLabel} ימים
                   </div>
-                  <div>
+                  <div onClick={this.editLeaseDates}>
                     + הוספת מועדי תחילת ותום שכירות
                   </div>
                   <div>
@@ -37,7 +73,7 @@ class PropertyManage extends Component {
                   </div>
                   <div>
                     תחילת שכירות
-                    {daysPassedLabel} ימים נותרו
+                    {daysLeftLabel} ימים נותרו
                     תום שכירות
                   </div>
                 </Col>
