733da59a3d183dc2f3c98c31b4fe397274e147ae
Idan Langer <lxngxr@users.noreply.github.com>
2017-04-27 14:54:08 +0300

    Bug referencing dorbel_user_id instead of id after auth0 user profile… (#943)
    * Bug referencing dorbel_user_id instead of id after auth0 user profile minimization
    
    closes  #942
    
    * missed one

--- front-gateway/src/providers/auth/AuthProvider.client.js
+++ front-gateway/src/providers/auth/AuthProvider.client.js
@@ -86,9 +86,9 @@ class AuthProvider {
   reportIdentifyAnalytics(profile) {
     // https://segment.com/docs/integrations/intercom/#identify
     if (profile) {
-      window.analytics.identify(profile.dorbel_user_id, profile);
+      window.analytics.identify(profile.id, profile);
 
-      if (profile.app_metadata && profile.app_metadata.first_login) {
+      if (profile.first_login) {
         window.analytics.track('client_signup_conversion'); // For Facebook conversion tracking.
       }
     }
