a58df1a733241853265760e4a104e0a96a0fd15f
Amos Lande <amos@dorbel.com>
2017-05-17 10:39:58 +0300

    Added TODO

--- front-gateway/src/providers/ModalProvider.js
+++ front-gateway/src/providers/ModalProvider.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
+// TODO: Simplify and cleanup the close handlers and delegates mess
 export default class ModalProvider {
   constructor(appStore) {
     this.appStore = appStore;
