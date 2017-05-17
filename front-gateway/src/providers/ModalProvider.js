7edb14d2a8dfc8d0f688c474ce653e10e84c2ebb
Amos Lande <amos@dorbel.com>
2017-05-17 10:06:23 +0300

    OHERegisterModal switch to DorbelModal usage

--- front-gateway/src/providers/ModalProvider.js
+++ front-gateway/src/providers/ModalProvider.js
@@ -18,7 +18,7 @@ export default class ModalProvider {
           <Button onClick={() => this.close(resolve, true)} bsStyle={params.confirmStyle || 'danger'} block>{params.confirmButton || 'המשך'}</Button>
           <Button onClick={() => this.close(resolve, false)} block>{params.cancelButton || 'ביטול'}</Button>
         </div>
-      )
+      );
 
       this.show(params, () => close(false), 'text-center');
     });
@@ -38,16 +38,16 @@ export default class ModalProvider {
 
   show(params, closeHandler, bodyClass) {
     this.appStore.modalParams = {
-        title: params.title,
-        body: (
+      title: params.title,
+      body: (
           <div className={bodyClass}>
             { params.heading && (<h4>{params.heading}</h4>) }
             { params.body }
           </div>
         ),
-        footer: params.footer,
-        modalSize: params.modalSize || 'small',
-        onClose: closeHandler};
+      footer: params.footer,
+      modalSize: params.modalSize || 'small',
+      onClose: closeHandler};
 
     this.appStore.showModal = true;
   }
