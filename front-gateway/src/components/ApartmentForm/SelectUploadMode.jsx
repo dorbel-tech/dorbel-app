4aa8762736011bf10a0cd517b6f157893c205357
Idan Langer <lxngxr@gmail.com>
2017-05-22 19:25:30 +0300

    upload type page design implementation - beginning

--- front-gateway/src/components/ApartmentForm/SelectUploadMode.jsx
+++ front-gateway/src/components/ApartmentForm/SelectUploadMode.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 import autobind from 'react-autobind';
 import { Col, Grid, Row } from 'react-bootstrap';
-import { inject, observer } from 'mobx-react';
-import UploadApartmentBaseStep from './UploadApartmentBaseStep';
+import { inject } from 'mobx-react';
+import Dashboard from '../Dashboard/Dashboard';
+import '../Dashboard/Dashboard.scss';
 
-@inject('appStore', 'appProviders') @observer
-class SelectuploadMode extends UploadApartmentBaseStep.wrappedComponent {
+
+@inject('appProviders')
+class SelectUploadMode extends Component {
   constructor(props) {
     super(props);
     this.navProvider = this.props.appProviders.navProvider;
@@ -14,29 +16,40 @@ class SelectuploadMode extends UploadApartmentBaseStep.wrappedComponent {
 
   render() {
     return (
-      <Grid fluid className="upload-apt-wrapper">
+      <Grid fluid className="select-upload-mode-wrapper">
         <Row>
-          <Col xs={12} className="upload-apt-left-container apartment-pictures-step">
-            <Col xs={12} md={6}>
-              <a href="/apartments/new_form/manage" onClick={this.navProvider.handleHrefClick}>
-                ניהול
-              </a>
-            </Col>
-            <Col xs={12} md={6}>
-              <a href="/apartments/new_form/publish" onClick={this.navProvider.handleHrefClick}>
-                פרסום
-              </a>
-            </Col>
+          <Col xs={5}>
+            <Dashboard />
           </Col>
+          <Col xs={7}>
+            <Row>
+              <h3>האם אתם מעלים דירה להשכרה או דירה לניהול?</h3>
+              <h4>בעוד 2 דק׳ תסיימו להעלות את הדירה לאתר! בחרו איזו דירה ברצונכם להעלות</h4>
+            </Row>
+            <Row>
+              <Row>
+                <Col xs={12} md={6}>
+                  <a href="/apartments/new_form/manage" onClick={this.navProvider.handleHrefClick} className="select-upload-mode-item">
+                    ניהול
+                </a>
+                </Col>
+                <Col xs={12} md={6}>
+                  <a href="/apartments/new_form/publish" onClick={this.navProvider.handleHrefClick} className="select-upload-mode-item">
+                    פרסום
+                </a>
+                </Col>
+              </Row>
+            </Row>
+          </Col>
+
         </Row>
       </Grid>
     );
   }
 }
 
-SelectuploadMode.wrappedComponent.propTypes = {
-  appProviders: React.PropTypes.object,
-  appStore: React.PropTypes.object
+SelectUploadMode.wrappedComponent.propTypes = {
+  appProviders: React.PropTypes.object
 };
 
-export default SelectuploadMode;
+export default SelectUploadMode;
