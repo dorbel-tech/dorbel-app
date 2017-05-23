d4ed87660bda8975a1aca8ef82009c570d3994ca
Idan Langer <lxngxr@gmail.com>
2017-05-23 13:07:42 +0300

    DONE!!!

--- front-gateway/src/components/ApartmentForm/SelectUploadMode.jsx
+++ front-gateway/src/components/ApartmentForm/SelectUploadMode.jsx
@@ -3,47 +3,75 @@ import autobind from 'react-autobind';
 import { Col, Grid, Row } from 'react-bootstrap';
 import { inject } from 'mobx-react';
 import Dashboard from '../Dashboard/Dashboard';
-import '../Dashboard/Dashboard.scss';
 
 
 @inject('appProviders')
 class SelectUploadMode extends Component {
+  static hideFooter = true;
+
   constructor(props) {
     super(props);
-    this.navProvider = this.props.appProviders.navProvider;
+    this.handleHrefClick = this.props.appProviders.navProvider.handleHrefClick;
     autobind(this);
   }
 
   render() {
     return (
-      <Grid fluid className="select-upload-mode-wrapper">
-        <Row>
-          <Col xs={2}>
-            <Dashboard />
-          </Col>
-          <Col xs={10}>
-            <Row>
-              <h3>האם אתם מעלים דירה להשכרה או דירה לניהול?</h3>
-              <h4>בעוד 2 דק׳ תסיימו להעלות את הדירה לאתר! בחרו איזו דירה ברצונכם להעלות</h4>
-            </Row>
-            <Row>
-              <Row>
-                <Col xs={12} md={6}>
-                  <a href="/apartments/new_form/manage" onClick={this.navProvider.handleHrefClick} className="select-upload-mode-item">
-                    ניהול
-                </a>
-                </Col>
-                <Col xs={12} md={6}>
-                  <a href="/apartments/new_form/publish" onClick={this.navProvider.handleHrefClick} className="select-upload-mode-item">
-                    פרסום
-                </a>
-                </Col>
-              </Row>
-            </Row>
-          </Col>
-
-        </Row>
-      </Grid>
+      <Dashboard externalComponent={
+        <Grid fluid >
+          <Row>
+            <Col>
+              <Col className="select-upload-mode-wrapper">
+                <Row className="select-upload-mode-header">
+                  <div className="select-upload-mode-title">האם אתם מעלים דירה להשכרה או דירה לניהול?</div>
+                  <div className="select-upload-mode-subtitle">בעוד 2 דק׳ תסיימו להעלות את הדירה לאתר! בחרו איזו דירה ברצונכם להעלות</div>
+                </Row>
+                <Row>
+                  <Row className="select-upload-mode-items-wrapper">
+                    <Col md={6}>
+                      <a href="/apartments/new_form/publish" onClick={this.handleHrefClick} className="select-upload-mode-item">
+                        <div className="select-upload-mode-item-icon">
+                          +
+                        </div>
+                        <div className="select-upload-mode-item-text">
+                          פרסום דירה להשכרה
+                        </div>
+                        <div className="select-upload-mode-item-subtext">
+                          (מחפשים דיירים חדשים)
+                        </div>
+                      </a>
+                    </Col>
+                    <Col md={6}>
+                      <a href="/apartments/new_form/manage" onClick={this.handleHrefClick} className="select-upload-mode-item">
+                        <div className="select-upload-mode-item-icon">
+                          +
+                        </div>
+                        <div className="select-upload-mode-item-text">
+                          העלאת דירה לניהול
+                        </div>
+                        <div className="select-upload-mode-item-subtext">
+                          (יש לי דיירים קיימים)
+                        </div>
+                      </a>
+                    </Col>
+                  </Row>
+                </Row>
+                <Row>
+                  <Row className="select-upload-mode-footer">
+                    <Col xs={12}>
+                      <div className="select-upload-mode-footer">
+                        לפרסום דירה קיימת היכנסו ל
+                         <a href="/dashboard/my-properties" onClick={this.handleHrefClick}>נכסים שלי&nbsp;</a>
+                        ובחרו בנכס הרלוונטי
+                        </div>
+                    </Col>
+                  </Row>
+                </Row>
+              </Col>
+            </Col>
+          </Row>
+        </Grid>
+      } />
     );
   }
 }
