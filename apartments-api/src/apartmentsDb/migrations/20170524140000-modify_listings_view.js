3871b67181f3e7dadce46351a213e567cea735c4
Idan Langer <lxngxr@gmail.com>
2017-05-24 15:31:36 +0300

    Change latest listings view

---
+++ apartments-api/src/apartmentsDb/migrations/20170524140000-modify_listings_view.js
@@ -0,0 +1,49 @@
+'use strict';
+
+/* 
+
+  This migration was in order to solve an issue with the previous view definition:
+    * The case:
+      - a listing was created for publishing (ID == 1)
+      - then the same apartment was uploaded for management with a past date (ID == 2)
+    * The result:
+      - when searching, the listing for publishing (ID == 1) would not return in the search results
+
+    * The solution is to create the view based on:
+      - Querying listings table in order to get a collection of: apartment_id, MAX(lease_end)
+      - Using the collection above, we query listings again by joining on apartment_id AND lease end
+
+*/
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.sequelize.query(
+      'DROP VIEW latest_listings', { type: Sequelize.QueryTypes.RAW })
+      .then(() => {
+        return queryInterface.sequelize.query(
+          'CREATE VIEW latest_listings AS '+
+              'SELECT listings.* '+ 
+              'FROM '+
+                  '(SELECT apartment_id, MAX( lease_end ) as lease_end '+
+                  'FROM listings '+
+                  'GROUP BY apartment_id) as apt_id_lease_end ' +
+              'INNER JOIN listings ON ' +
+                  'listings.apartment_id = apt_id_lease_end.apartment_id ' +
+                  'AND listings.lease_end = apt_id_lease_end.lease_end ' +
+              'WHERE listings.status <> \'deleted\''
+        , { type: Sequelize.QueryTypes.RAW });
+      });
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.sequelize.query(
+      'DROP VIEW latest_listings', { type: Sequelize.QueryTypes.RAW })
+      .then(()=>{
+        return queryInterface.sequelize.query(
+          'CREATE VIEW latest_listings AS SELECT * FROM listings WHERE id IN ( ' +
+          'SELECT MAX(id) FROM listings WHERE status != \'deleted\'' +
+          'GROUP BY apartment_id' +
+          ')', { type: Sequelize.QueryTypes.RAW });
+      });
+  }
+};
