1e89b193c60638497c77e87c2ad9b9c4f455a1e6
Idan Langer <lxngxr@users.noreply.github.com>
2017-05-25 16:09:32 +0300

    Fix selectUploadMode view on mobile (#1060)

--- front-gateway/src/components/ApartmentForm/SelectUploadMode.jsx
+++ front-gateway/src/components/ApartmentForm/SelectUploadMode.jsx
@@ -18,52 +18,42 @@ class SelectUploadMode extends Component {
   render() {
     return (
       <Dashboard externalComponent={
-        <Grid fluid >
-          <Row>
-            <Col>
-              <Col className="select-upload-mode-wrapper">
-                <Row className="select-upload-mode-header">
-                  <div className="select-upload-mode-title">האם אתם מעלים דירה להשכרה או דירה לניהול?</div>
-                  <div className="select-upload-mode-subtitle">בעוד 2 דק׳ תסיימו להעלות את הדירה לאתר! בחרו איזו דירה ברצונכם להעלות</div>
-                </Row>
-                <Row>
-                  <Row className="select-upload-mode-items-wrapper">
-                    <Col md={6}>
-                      <a href="/apartments/new_form/publish" onClick={this.handleHrefClick} className="select-upload-mode-item">
-                        <img src={UPLOAD_IMAGE_URL} className="select-upload-mode-item-icon"/>
-                        <div className="select-upload-mode-item-text">
-                          פרסום דירה להשכרה
-                        </div>
-                        <div className="select-upload-mode-item-subtext">
-                          (מחפשים דיירים חדשים)
-                        </div>
-                      </a>
-                    </Col>
-                    <Col md={6}>
-                      <a href="/apartments/new_form/manage" onClick={this.handleHrefClick} className="select-upload-mode-item">
-                        <img src={UPLOAD_IMAGE_URL} className="select-upload-mode-item-icon"/>
-                        <div className="select-upload-mode-item-text">
-                          העלאת דירה לניהול
-                        </div>
-                        <div className="select-upload-mode-item-subtext">
-                          (יש לי דיירים קיימים)
-                        </div>
-                      </a>
-                    </Col>
-                  </Row>
-                </Row>
-                <Row>
-                  <Row className="select-upload-mode-footer">
-                    <Col xs={12}>
-                      <div className="select-upload-mode-footer">
-                        לפרסום דירה קיימת היכנסו ל-&nbsp;
-                         <a href="/dashboard/my-properties" onClick={this.handleHrefClick}>נכסים שלי</a>
-                        , בחרו בנכס הרלוונטי ופרסמו אותו מחדש.
-                        </div>
-                    </Col>
-                  </Row>
-                </Row>
-              </Col>
+        <Grid fluid className="select-upload-mode-wrapper">
+          <Row className="select-upload-mode-header">
+            <div className="select-upload-mode-title">האם אתם מעלים דירה להשכרה או דירה לניהול?</div>
+            <div className="select-upload-mode-subtitle">בעוד 2 דק׳ תסיימו להעלות את הדירה לאתר! בחרו איזו דירה ברצונכם להעלות</div>
+          </Row>
+          <Row className="select-upload-mode-items-wrapper">
+            <Col md={6}>
+              <a href="/apartments/new_form/publish" onClick={this.handleHrefClick} className="select-upload-mode-item">
+                <img src={UPLOAD_IMAGE_URL} className="select-upload-mode-item-icon" />
+                <div className="select-upload-mode-item-text">
+                  פרסום דירה להשכרה
+                </div>
+                <div className="select-upload-mode-item-subtext">
+                  (מחפשים דיירים חדשים)
+                </div>
+              </a>
+            </Col>
+            <Col md={6}>
+              <a href="/apartments/new_form/manage" onClick={this.handleHrefClick} className="select-upload-mode-item">
+                <img src={UPLOAD_IMAGE_URL} className="select-upload-mode-item-icon" />
+                <div className="select-upload-mode-item-text">
+                  העלאת דירה לניהול
+                </div>
+                <div className="select-upload-mode-item-subtext">
+                  (יש לי דיירים קיימים)
+                </div>
+              </a>
+            </Col>
+          </Row>
+          <Row className="select-upload-mode-footer">
+            <Col xs={12}>
+              <div className="select-upload-mode-footer">
+                לפרסום דירה קיימת היכנסו ל-&nbsp;
+                <a href="/dashboard/my-properties" onClick={this.handleHrefClick}>נכסים שלי</a>
+                , בחרו בנכס הרלוונטי ופרסמו אותו מחדש.
+              </div>
             </Col>
           </Row>
         </Grid>
