b0ab6a3bee3795ee6b49f96e967b6064d2d81401
David Virtser <david@dorbel.com>
2017-05-30 11:56:43 +0300

    typo fix

--- apartments-api/src/services/likeService.js
+++ apartments-api/src/services/likeService.js
@@ -24,7 +24,7 @@ function* getByListing(listingId, user, include_profile) {
       // Get all the data about who liked a listing.
       likes.forEach((like) => {
         const promiseForUser = userManagement.getPublicProfile(like.following_user_id)
-          .then(user_details => { likes.user_details = user_details; });
+          .then(user_details => { like.user_details = user_details; });
         promises.push(promiseForUser);
       });
     }
