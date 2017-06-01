03abccfa65084099b710504656fc72cb5808eb0b
Idan Langer <lxngxr@gmail.com>
2017-06-01 14:05:40 +0300

    Will this work!? :)

--- apartments-api/src/apartmentsDb/migrations/20170524140000-modify_listings_view.2.js
+++ apartments-api/src/apartmentsDb/migrations/20170524140000-modify_listings_view.2.js
@@ -20,11 +20,7 @@ module.exports = {
     return queryInterface.sequelize.query(
         'ALTER VIEW latest_listings AS '+
             'SELECT listings.* '+ 
-            'FROM '+
-                '(SELECT apartment_id, MAX( lease_end ) as lease_end '+
-                'FROM listings '+
-                'WHERE listings.status <> \'deleted\' '+
-                'GROUP BY apartment_id) as apt_id_lease_end ' +
+            'FROM apt_id_lease_end ' +
             'INNER JOIN listings ON ' +
                 'listings.apartment_id = apt_id_lease_end.apartment_id ' +
                 'AND listings.lease_end = apt_id_lease_end.lease_end'
