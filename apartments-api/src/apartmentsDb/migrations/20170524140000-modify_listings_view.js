7403bf965fed59ac50c9124fb1b70582b9698d48
Idan Langer <lxngxr@users.noreply.github.com>
2017-06-01 10:16:01 +0300

    Fix new_listings view migration (#1081)
    * Fix new_listings view migration
    
    1. Using `ATLER VIEW` instead of `DROP` and `CREATE`
    1. Separate index creation to a new migration
    
    * missed a spot...

--- apartments-api/src/apartmentsDb/migrations/20170524140000-modify_listings_view.js
+++ apartments-api/src/apartmentsDb/migrations/20170524140000-modify_listings_view.js
@@ -18,39 +18,25 @@
 module.exports = {
   up: function (queryInterface, Sequelize) {
     return queryInterface.sequelize.query(
-      'DROP VIEW latest_listings', { type: Sequelize.QueryTypes.RAW })
-      .then(() => {
-        return queryInterface.sequelize.query(
-          'CREATE VIEW latest_listings AS '+
-              'SELECT listings.* '+ 
-              'FROM '+
-                  '(SELECT apartment_id, MAX( lease_end ) as lease_end '+
-                  'FROM listings '+
-                  'WHERE listings.status <> \'deleted\' '+
-                  'GROUP BY apartment_id) as apt_id_lease_end ' +
-              'INNER JOIN listings ON ' +
-                  'listings.apartment_id = apt_id_lease_end.apartment_id ' +
-                  'AND listings.lease_end = apt_id_lease_end.lease_end '
+        'ALTER VIEW latest_listings AS '+
+            'SELECT listings.* '+ 
+            'FROM '+
+                '(SELECT apartment_id, MAX( lease_end ) as lease_end '+
+                'FROM listings '+
+                'WHERE listings.status <> \'deleted\' '+
+                'GROUP BY apartment_id) as apt_id_lease_end ' +
+            'INNER JOIN listings ON ' +
+                'listings.apartment_id = apt_id_lease_end.apartment_id ' +
+                'AND listings.lease_end = apt_id_lease_end.lease_end '
         , { type: Sequelize.QueryTypes.RAW });
-      })
-      .then(()=>{
-        return queryInterface.addIndex('listings', ['apartment_id', 'lease_end']);
-      });
   },
 
   down: function (queryInterface, Sequelize) {
     return queryInterface.sequelize.query(
-      'DROP VIEW latest_listings', { type: Sequelize.QueryTypes.RAW })
-      .then(()=>{
-        return queryInterface.sequelize.query(
-          'CREATE VIEW latest_listings AS SELECT * FROM listings WHERE id IN ( ' +
-          'SELECT MAX(id) FROM listings WHERE status != \'deleted\'' +
-          'GROUP BY apartment_id' +
-          ')', { type: Sequelize.QueryTypes.RAW }
+      'ALTER VIEW latest_listings AS SELECT * FROM listings WHERE id IN ( ' +
+      'SELECT MAX(id) FROM listings WHERE status != \'deleted\'' +
+      'GROUP BY apartment_id' +
+      ')', { type: Sequelize.QueryTypes.RAW }
           );
-      })
-      .then(()=>{
-        return queryInterface.removeIndex('listings', ['apartment_id', 'lease_end']);
-      });
   }
 };
