b4826080550ca34e537549776d979ba2c1a955f7
David Virtser <david@dorbel.com>
2017-06-18 19:21:41 +0300

    Republish apartment flow missing creation of OHE as part of it #1160

--- front-gateway/src/providers/ListingsProvider.js
+++ front-gateway/src/providers/ListingsProvider.js
@@ -63,7 +63,7 @@ class ListingsProvider {
     return this.apiProvider.fetch('/api/apartments/v1/listings', { method: 'POST', data: listing })
       .then((newListing) => createdListing = newListing)
       .then(() => { // TODO: move OHE creation to pub/sub messages on background
-        if (uploadMode == 'publish') {
+        if (uploadMode == 'publish' || uploadMode == 'republish') {
           try {
             this.oheProvider.createOhe(Object.assign({ listing_id: createdListing.id }, listing.open_house_event));
           } catch (err) { /*eslint-disable eslint-enable*/ }
