18ff07e08412bc8627fa54cdd07ec394222a7f1b
David Virtser <david@dorbel.com>
2017-06-20 10:46:45 +0300

    removed console

--- apartments-api/test/integration/utils.js
+++ apartments-api/test/integration/utils.js
@@ -3,7 +3,6 @@ function* clearAllUserLikes(apiClient) {
 
   for (let i = 0; i < existingLikesResponse.body.length; i++) {
     let userLike = existingLikesResponse.body[i];
-    console.log('userLike', userLike);
     yield apiClient.unlikeApartment(userLike.apartment_id, userLike.listing_id).expect(200).end();
   }
 }
