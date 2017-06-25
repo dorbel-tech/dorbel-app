7a3d1e86f3e0adb431387817592df4e9898c12a1
Amos Lande <amos@dorbel.com>
2017-06-25 16:08:27 +0300

    Cleanup and doc

--- front-gateway/src/providers/MessagingProvider.js
+++ front-gateway/src/providers/MessagingProvider.js
@@ -1,21 +1,20 @@
 'use strict';
 
+// TalkJS wrapper provider, see docs: https://talkjs.com/docs/index.html
 class MessagingProvider {
   constructor(authStore) {
     this.authStore = authStore;
 
-    this.initTalkJS();
-  }
-
-  initTalkJS() {
     // will only work on client side
     global.window &&
       this.talkjs(global.window, document, []) &&
       this.initTalkSession();
   }
 
+  // If active user is logged in and an active TalkJS user was not
+  // already created, create an active TalkJS user using the logged in user profile.
   initTalkUser() {
-    if (this.authStore.isLoggedIn) {
+    if (!this.talkUser && this.authStore.isLoggedIn) {
       this.talkUser = new global.window.Talk.User({
         id: this.authStore.profile.dorbel_user_id,
         name: this.authStore.profile.first_name,
@@ -31,6 +30,7 @@ class MessagingProvider {
     return false;
   }
 
+  // If an active TalkJS user was created, create a new TalkJS session.
   initTalkSession() {
     if (!this.initTalkUser()) {
       return false;
@@ -45,11 +45,14 @@ class MessagingProvider {
     return true;
   }
 
+  // TalkJS loader script, which creates a global.window.Talk object.
   talkjs(t,a,l,k,j,s) {
     s=a.createElement('script');s.async=1;s.src='https://cdn.talkjs.com/talk.js';a.getElementsByTagName('head')[0].appendChild(s);k=t.Promise;
     t.Talk={ready:{then:function(f){if(k){return new k(function(r,e){l.push([f,r,e]);});}l.push([f]);},catch:function(){return k&&new k();},c:l}};
   }
 
+  // Create a TalkJS user and start a conversation between the active
+  // TalkJS user and the newly created TalkJS user using a new TalkJS session.
   getOrStartConversation(withUserObj, options) {
     if (this.initTalkSession()) {
       const withUser = new global.window.Talk.User(withUserObj);
