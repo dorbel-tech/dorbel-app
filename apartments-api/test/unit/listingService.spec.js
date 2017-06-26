38f57f3034e71377eb070002aa536d7a79abfa9a
Avner Sorek <avner.sorek@gmail.com>
2017-06-26 14:17:09 +0300

    test fix

--- apartments-api/test/unit/listingService.spec.js
+++ apartments-api/test/unit/listingService.spec.js
@@ -68,22 +68,22 @@ describe('Listing Service', function () {
 
     it('should set default status of listed only', function* () {
       yield this.listingService.getByFilter();
-      __.assertThat(this.listingRepositoryMock.list.args[0][0], __.hasProperties({ status: 'listed' }));
+      __.assertThat(this.listingRepositoryMock.list.args[0][0], __.hasProperty('$and', __.contains({ status: 'listed' })));
     });
 
     it('should set conditions for future booking', function* () {
       yield this.listingService.getByFilter('{ "futureBooking": true }');
-      __.assertThat(this.listingRepositoryMock.list.args[0][0], __.allOf(
-        __.not(__.hasProperty('status')),
-        __.hasProperty('$or', __.contains(
+      __.assertThat(this.listingRepositoryMock.list.args[0][0], __.hasProperty('$and', __.allOf(
+        __.not(__.contains(__.hasProperty('status'))),
+        __.contains(__.hasProperty('$or', __.contains(
           __.hasProperty('status', 'listed'),
           __.hasProperties({
             status: 'rented',
             lease_end: __.hasProperty('$gte', __.is(__.date())),
             show_for_future_booking: true
           })
-        ))
-      ));
+        )))
+      )));
     });
 
     it('should throw an error for my-properties without user object', function* () {
