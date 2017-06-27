66a5a89052763cbe04bef890e2c410299c974276
Amos Lande <amos@dorbel.com>
2017-06-27 09:03:41 +0300

    TenantRow unit tests

--- front-gateway/src/components/Tenants/TenantRow/TenantRow.spec.js
+++ front-gateway/src/components/Tenants/TenantRow/TenantRow.spec.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import faker from 'faker';
 
+import utils from '~/providers/utils';
 import TenantRow from '~/components/Tenants/TenantRow/TenantRow';
 import TenantProfile from '~/components/Tenants/TenantProfile/TenantProfile';
 
@@ -16,7 +17,7 @@ describe('Tenant Row', () => {
     };
   });
 
-  const tenantRow = (tenant) => shallow(<TenantRow.wrappedComponent tenant={tenant} listingTitle="Listing Title" appProviders={appProvidersMock} />);
+  const tenantRow = (tenant, listingTitle) => shallow(<TenantRow.wrappedComponent tenant={tenant} listingTitle={listingTitle || 'Listing Title'} appProviders={appProvidersMock} />);
 
   it('should show tenant first name and last name', () => {
     const tenant = { first_name: faker.name.firstName(), last_name: faker.name.lastName() };
@@ -29,5 +30,51 @@ describe('Tenant Row', () => {
     wrapper.find('Col').first().simulate('click');
 
     expect(appProvidersMock.modalProvider.showInfoModal.mock.calls[0][0].body.type).toBe(TenantProfile);
+  });
+
+  it('should show disabled tenant row', () => {
+    const wrapper = tenantRow({ disabled: true });
+    wrapper.find('Col').first().simulate('click');
+
+    expect(appProvidersMock.modalProvider.showInfoModal).not.toHaveBeenCalled();
+  });
+
+  it('should show disabled tenant row', () => {
+    const tenant = { dorbel_user_id: faker.random.uuid(), listing_id: faker.random.number(), first_name: faker.name.firstName(), email: faker.internet.email() };
+    const listingTitle = faker.name.title();
+    const conversationMock = jest.fn();
+    const popupMock = {mount: jest.fn()};
+    appProvidersMock.messagingProvider = {
+      getOrStartConversation: jest.fn().mockReturnValue(conversationMock),
+      talkSession: {createPopup: jest.fn().mockReturnValue(popupMock)}
+    }
+    utils.hideIntercom = jest.fn();
+    let callbacks = [];
+    global.window.Talk = {ready: {then: (f) => callbacks.push(f)}};
+
+    const wrapper = tenantRow(tenant, listingTitle);
+    wrapper.find('.tenant-row-msg-icon').simulate('click');
+
+    expect(callbacks.length).toEqual(1);
+    callbacks[0]();
+
+    expect(appProvidersMock.messagingProvider.getOrStartConversation).toHaveBeenCalledWith(
+      {
+        id: tenant.dorbel_user_id,
+        name: tenant.first_name,
+        email: tenant.email,
+        configuration: 'general',
+        welcomeMessage: 'באפשרותך לשלוח הודעה לדיירים. במידה והם אינם מחוברים הודעתך תישלח אליהם למייל.'
+      },
+      {
+        topicId: tenant.listing_id,
+        subject: listingTitle
+      }
+    );
+    expect(appProvidersMock.messagingProvider.talkSession.createPopup).toHaveBeenCalledWith(
+      conversationMock
+    );
+    expect(popupMock.mount).toHaveBeenCalledWith();
+    expect(utils.hideIntercom).toHaveBeenCalledWith(true);
   });
 });
