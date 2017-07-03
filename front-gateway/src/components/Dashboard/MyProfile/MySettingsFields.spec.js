a3e443a087650d0d837a661af95a5d6150e00fb2
Amos Lande <amos@dorbel.com>
2017-07-03 11:10:54 +0300

    User settings unit test

---
+++ front-gateway/src/components/Dashboard/MyProfile/MySettingsFields.spec.js
@@ -0,0 +1,55 @@
+'use strict';
+import { shallow } from 'enzyme';
+import React from 'react';
+import { FRC } from '~/components/FormWrapper/FormWrapper';
+import MySettingsFields from './MySettingsFields';
+
+describe('MySettingsFields', () => {
+  let props;
+  let comp;
+
+  const mySettingsFields = () => {
+    if (!comp) {
+      comp = shallow(
+        <MySettingsFields {...props} />
+      );
+    }
+    return comp;
+  };
+
+  beforeEach(() => {
+    props = {
+      profile: {}
+    };
+    comp = undefined;
+  });
+
+  it('should initialize correctly', () => {
+    mySettingsFields();
+
+    expect(comp.find(FRC.Input).prop('value')).toEqual('settings');
+
+    const checkboxes = comp.find(FRC.Checkbox);
+    expect(checkboxes.length).toEqual(2);
+    checkboxes.forEach(checkbox => {
+      expect(checkbox.prop('value')).toEqual(true);
+    });
+  });
+
+  it('should set checkboxes values according to the given profile settings', () => {
+    const receiveNotificationsMock = jest.fn();
+    const receiveNewsletterMock = jest.fn();
+    const expectedCheckboxValues = [receiveNotificationsMock, receiveNewsletterMock];
+    props.profile.settings = {
+      receive_notifications: receiveNotificationsMock,
+      receive_newsletter: receiveNewsletterMock
+    }
+
+    mySettingsFields();
+
+    const checkboxes = comp.find(FRC.Checkbox);
+    checkboxes.forEach((checkbox, i) => {
+      expect(checkbox.prop('value')).toEqual(expectedCheckboxValues[i]);
+    });
+  });
+});
