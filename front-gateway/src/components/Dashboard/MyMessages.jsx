0b7cef8912b6d13798fb06d4f4bec395a5c81d2d
Amos Lande <amos@dorbel.com>
2017-07-04 18:23:32 +0300

    MyMessages hide intercom on mobile phone

--- front-gateway/src/components/Dashboard/MyMessages.jsx
+++ front-gateway/src/components/Dashboard/MyMessages.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
+import ismobilejs from 'ismobilejs';
+import { hideIntercom } from '~/providers/utils';
 
 import './MyMessages.scss';
 
@@ -13,6 +15,13 @@ class MyMessages extends Component {
     const { messagingProvider } = this.props.appProviders;
 
     messagingProvider.createInbox(this.talkjsContainer);
+    if (ismobilejs.phone) {
+      hideIntercom(true);
+    }
+  }
+
+  componentWillUnmount() {
+    hideIntercom(false);
   }
 
   render() {
