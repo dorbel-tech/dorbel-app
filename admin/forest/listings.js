69c511e2c962b75267d766adcba5f787987f4f6f
David Virtser <david@dorbel.com>
2017-07-09 14:40:44 +0300

    bug fix

--- admin/forest/listings.js
+++ admin/forest/listings.js
@@ -20,7 +20,7 @@ Liana.collection('listings', {
     field: 'listing_url',
     type: 'String',
     value: function (object) {
-      return process.env.FRONT_GATEWAY_URL + 'apartments/' + object.apartment_id;
+      return process.env.FRONT_GATEWAY_URL + 'apartments/' + object.id;
     }
   }]
 });
