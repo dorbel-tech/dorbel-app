3beb13aab8365a36d346bb977e37ee76210f9d63
David Virtser <david@dorbel.com>
2017-07-10 15:17:46 +0300

    removed unused share icons

--- front-gateway/src/components/Listing/components/ListingSocial.jsx
+++ front-gateway/src/components/Listing/components/ListingSocial.jsx
@@ -16,12 +16,10 @@ class ListingSocial extends React.Component {
       <Col>
         שתפו חברים שמחפשים:
         <div className="listing-social-share-container">
-          <a className="listing-social-share-item fa fa-facebook-f fb-desktop" href={'https://www.facebook.com/sharer.php?u=' + currentUrl + '?utm_source=app&utm_medium=share&utm_campaign=facebook'} target="_blank"></a>
-          <a className="listing-social-share-item fa fa-facebook-f fb-mobile" href={'fb://publish/profile/me?text=' + currentUrl + '?utm_source=app&utm_medium=share&utm_campaign=facebook'}></a>
-          <a className="listing-social-share-item email fa fa-envelope-o" href={'mailto:?subject=Great%20apartment%20from%20dorbel&amp;body=' + currentUrl + '?utm_source=app&utm_medium=share&utm_campaign=email'}></a>
-          <a className="listing-social-share-item whatsapp fa fa-whatsapp" href={'whatsapp://send?text=היי, ראיתי דירה באתר dorbel שאולי תעניין אותך. ' + currentUrl + '?utm_source=app&utm_medium=share&utm_campaign=whatsapp'} data-href={currentUrl + '?utm_source=app&utm_medium=share&utm_campaign=whatsapp'} data-text="היי, ראיתי דירה באתר dorbel שאולי תעניין אותך."></a>
-          <a className="listing-social-share-item fb-messenger-desktop" href={'https://www.facebook.com/dialog/send?app_id=1651579398444396&link=' + currentUrl + '?utm_source=app&utm_medium=share&utm_campaign=messenge' + '&redirect_uri=' + currentUrl + '?utm_source=app&utm_medium=share&utm_campaign=messenger'} target="_blank"><Icon iconName="dorbel-icon-social-fbmsg" /></a>
-          <a className="listing-social-share-item fb-messenger-mobile" href={'fb-messenger://share/?link=' + currentUrl + '?utm_source=app&utm_medium=share&utm_campaign=messenger' + '&app_id=1651579398444396'}><Icon iconName="dorbel-icon-social-fbmsg" /></a>
+          <a className="listing-social-share-item fa fa-facebook-f fb-desktop" href={'https://www.facebook.com/sharer.php?u=' + currentUrl + '?utm_source=app&utm_medium=share&utm_campaign=facebook_share'} target="_blank"></a>
+          <a className="listing-social-share-item fa fa-facebook-f fb-mobile" href={'fb://publish/profile/me?text=' + currentUrl + '?utm_source=app&utm_medium=share&utm_campaign=facebook_share'}></a>
+          <a className="listing-social-share-item whatsapp fa fa-whatsapp" href={'whatsapp://send?text=היי, ראיתי דירה באתר dorbel שאולי תעניין אותך. ' + currentUrl + '?utm_source=app&utm_medium=share&utm_campaign=whatsapp_share'} data-href={currentUrl + '?utm_source=app&utm_medium=share&utm_campaign=whatsapp_share'} data-text="היי, ראיתי דירה באתר dorbel שאולי תעניין אותך."></a>
+          <a className="listing-social-share-item fb-messenger-mobile" href={'fb-messenger://share/?link=' + currentUrl + '?utm_source=app&utm_medium=share&utm_campaign=messenger_share' + '&app_id=1651579398444396'}><Icon iconName="dorbel-icon-social-fbmsg" /></a>
         </div>
       </Col>
     );
