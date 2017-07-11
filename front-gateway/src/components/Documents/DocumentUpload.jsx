67b8e54c11a965d61917b0b42930ee9c855e2e19
Avner Sorek <avner.sorek@gmail.com>
2017-07-11 17:31:50 +0300

    added upload to S3 specific path

--- front-gateway/src/components/Documents/DocumentUpload.jsx
+++ front-gateway/src/components/Documents/DocumentUpload.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import autobind from 'react-autobind';
 import { inject } from 'mobx-react';
 import { Button } from 'react-bootstrap';
+import _ from 'lodash';
 import './DocumentUpload.scss';
 
 let ReactFilestack = 'div';
@@ -9,24 +10,41 @@ let ReactFilestack = 'div';
 const FILESTACK_OPTIONS = { 
   fromSources: [ 'local_file_system','gmail','googledrive','dropbox','box' ], 
   disableTransformer: true, 
-  maxFiles: 10 
+  maxFiles: 10,
+  storeTo: {
+    location: 's3',
+    region: 'eu-west-1',
+    container: 'dorbel-user-documents'
+  }
 };
 
 if (process.env.IS_CLIENT) {
   ReactFilestack = require('filestack-react').default;
 }
 
-@inject('appProviders')
+@inject('appProviders', 'appStore')
 export default class DocumentUpload extends React.Component {
   constructor(props) {
     super(props);
     autobind(this);
+
+    this.uploadOptions = this.getUploadOptions(props);
   }
 
   static propTypes = {
     appProviders: React.PropTypes.object,
+    appStore: React.PropTypes.object,
     className: React.PropTypes.string,
     listing_id: React.PropTypes.number.isRequired
+  }
+
+  getUploadOptions(props) {
+    const env = process.env.NODE_ENV;
+    const user_id = props.appStore.authStore.profile.dorbel_user_id;
+    const listing_id = props.listing_id;
+    const uploadOptions = _.cloneDeep(FILESTACK_OPTIONS);
+    uploadOptions.storeTo.path = `${env}/${user_id}/${listing_id}/`;
+    return uploadOptions;
   }
 
   onDocumentUploaded(response) {
@@ -41,13 +59,13 @@ export default class DocumentUpload extends React.Component {
     if (!process.env.FILESTACK_API_KEY) {
       return null;
     }
-    
+
     return (
       <ReactFilestack
         apikey={process.env.FILESTACK_API_KEY}
         onSuccess={this.onDocumentUploaded}
         render={this.renderButton}
-        options={FILESTACK_OPTIONS}
+        options={this.uploadOptions}
       />
     );
   }
