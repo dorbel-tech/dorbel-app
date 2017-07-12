4dc157ff8d5183a2c330890e18707e192abce290
David Virtser <poison77@gmail.com>
2017-07-12 11:03:44 +0300

    Move landlord communication buttons under OHEs, remove phone and move manage button (#1301)
    * Move landlord communication buttons under OHEs #1293
    
    * cleanup
    
    * PR fixes
    
    * Adding more tweaks
    
    * more changes
    
    * more small visual changes
    
    * final touch
    
    * lint fixes
    
    * bug fix
    
    * lint fix
    
    * removed show_phone option
    
    * added tracking
    
    * lin fix

--- front-gateway/src/components/ApartmentForm/UploadApartmentStep3.jsx
+++ front-gateway/src/components/ApartmentForm/UploadApartmentStep3.jsx
@@ -52,7 +52,7 @@ class UploadApartmentStep3 extends UploadApartmentBaseStep.wrappedComponent {
 
     if (authStore.isLoggedIn) {
       // setting this up specificially because somehow it gets lost when logging in
-      const { publishing_user_type, show_phone } = this.props.appStore.newListingStore.formValues.publishing_user_type;
+      const { publishing_user_type } = this.props.appStore.newListingStore.formValues.publishing_user_type;
 
       return (
         <Row className="form-section">
@@ -74,9 +74,6 @@ class UploadApartmentStep3 extends UploadApartmentBaseStep.wrappedComponent {
               </Col>
               <Col md={6}>
                 <FRC.Input validations="isNumeric" name="user.phone" label="טלפון" value={authStore.profile.phone} validationError="מספר טלפון לא תקין" required />
-                <div className="is-phone-visible-input">
-                  <FRC.Checkbox name="show_phone" label="הציגו את המספר שלי במודעה" value={show_phone} />
-                </div>
               </Col>
             </Row>
             <Row>
