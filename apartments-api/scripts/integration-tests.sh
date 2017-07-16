16b14f57c2c9cb42c0bc7c14a8060f261afa4ee8
Idan Langer <lxngxr@gmail.com>
2017-07-16 12:34:53 +0300

    script

--- apartments-api/scripts/integration-tests.sh
+++ apartments-api/scripts/integration-tests.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-RET_CODE = 0
+EXIT_CODE=0
+
 function runTests() {
   yarn run wait && mocha --timeout 10000 --compilers js:babel-register test/shared/before-tests.js test/integration/**/*.spec.js;
-  RET_CODE = $?
+  EXIT_CODE=$? # Keep the exit code
 }
 
 runTests
-node "$DIR/dbCleaner.js"
-exit $RET_CODE
+node "$DIR/dbCleaner.js" # Clean the DB
+
+exit ${RET_CODE}
