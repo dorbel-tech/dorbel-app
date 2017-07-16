8a35dc6d716ebeb5e9b7585b549770cf2c9bce1c
Idan Langer <lxngxr@gmail.com>
2017-07-16 16:39:58 +0300

    2 sperate scripts + replace test-seed user on likes

--- apartments-api/scripts/integration-tests.sh
+++ apartments-api/scripts/integration-tests.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # Get the scripts folder location in order to run dbCleaner 
-                                                        # This way this script can be called from anywhere and find dbCleaner.js
 EXIT_CODE=0
 
 function runTests() {
   yarn run wait && mocha --timeout 10000 --compilers js:babel-register test/shared/before-tests.js test/integration/**/*.spec.js;
-  EXIT_CODE=$? # Keep the exit code
+  EXIT_CODE=$? # Keep the mocha exit code in order to return it at the end
 }
 
 runTests
-node "$DIR/dbCleaner.js" # Clean the DB
+yarn db:clean
 
 exit ${EXIT_CODE}
