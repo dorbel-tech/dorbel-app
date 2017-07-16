91052d78396ce78f35ea9bf2214b7c22dee40387
Idan Langer <lxngxr@gmail.com>
2017-07-16 17:10:22 +0300

    update comment

--- apartments-api/scripts/clean-db.sh
+++ apartments-api/scripts/clean-db.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # Get the scripts folder location in order to run dbCleaner
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # Get the scripts folder location
                                                         # This way this script can be called from anywhere and find dbCleaner.js
 echo "Cleaning the database"
 node $DIR/dbCleaner.js
