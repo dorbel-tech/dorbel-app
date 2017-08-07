34cab83901ead022c8eb53f40fc4a5601f96a7a8
Idan Langer <lxngxr@gmail.com>
2017-08-07 18:46:01 +0300

    fix integration-tests.sh to not use babel-register compiler

--- apartments-api/scripts/integration-tests.sh
+++ apartments-api/scripts/integration-tests.sh
@@ -2,7 +2,7 @@
 EXIT_CODE=0
 
 function runTests() {
-  yarn run wait && mocha --timeout 10000 --compilers js:babel-register test/shared/before-tests.js test/integration/**/*.spec.js;
+  yarn run wait && mocha --timeout 10000 test/shared/before-tests.js test/integration/**/*.spec.js;
   EXIT_CODE=$? # Keep the mocha exit code in order to return it at the end
 }
 
