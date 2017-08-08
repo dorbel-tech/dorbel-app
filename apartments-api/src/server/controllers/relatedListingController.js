cc141ee9e759c830793d105b30e4aa77b4108ba8
Avner Sorek <avner.sorek@gmail.com>
2017-08-08 14:55:40 +0300

    fixing tests

--- apartments-api/src/server/controllers/relatedListingController.js
+++ apartments-api/src/server/controllers/relatedListingController.js
@@ -4,11 +4,12 @@ const shared = require('dorbel-shared');
 const ONE_HOUR = 60 * 60;
 
 async function get(ctx) {
-  const apartmentId = parseInt(ctx.params.apartmentId);
+  // Written as params.listingId because all params under the route /listings/{paramName} have to be the same name
+  const apartmentId = parseInt(ctx.params.listingId);
   const NUMBER_OF_ITEMS = 3;
   const relatedListings = await listingService.getRelatedListings(apartmentId, NUMBER_OF_ITEMS);
 
-  if(relatedListings){
+  if (relatedListings){
     shared.helpers.headers.setCacheHeader(ctx.response, ONE_HOUR);
     ctx.response.body = relatedListings;
   }
