fc156654d59d098a759d29672d34d6a296bc7990
Avner Sorek <avner.sorek@gmail.com>
2017-08-08 18:21:18 +0300

    removing generators from GQL resolvers

--- apartments-api/src/server/graphql/resolvers.js
+++ apartments-api/src/server/graphql/resolvers.js
@@ -3,9 +3,8 @@ const cityRepository = require('../../apartmentsDb/repositories/cityRepository')
 const neighborhoodRepository = require('../../apartmentsDb/repositories/neighborhoodRepository');
 const listingService = require('../../services/listingService');
 const documentService = require('../../services/documentService');
-const { applyCoToGeneratorResolvers } = require('./coResolvers');
 
-// resolver signiture is function(root-object, query-params, context, metadata) : returns value or promise
+// resolver signature is function(root-object, query-params, context, metadata) and should return value or promise
 const resolvers = {
   Query: {
     cities: cityRepository.list,
@@ -14,10 +13,8 @@ const resolvers = {
     listings: (_, params, context) => listingService.getByFilter(params, { user: context.user })
   },
   Listing: {
-    documents: async function (listing, params, context) {
-      return await documentService.getByListingId(listing.id, context.user);
-    }
+    documents: (listing, params, context) => documentService.getByListingId(listing.id, context.user)
   }
 };
 
-module.exports = applyCoToGeneratorResolvers(resolvers);
+module.exports = resolvers;
