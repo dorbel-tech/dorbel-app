0045da68c0db3503551f8d7ebc63bd233bc62a4f
Idan Langer <lxngxr@gmail.com>
2017-08-14 18:09:34 +0300

    addressUtil

--- apartments-api/src/services/utils/addressUtils.js
+++ apartments-api/src/services/utils/addressUtils.js
@@ -1,66 +1,83 @@
 'use-strict';
 const errors = require('dorbel-shared').utils.domainErrors;
+const _ = require('lodash');
 
 const mapsApiClient = require('@google/maps').createClient({
   key: process.env.GOOGLE_MAPS_API_KEY,
   Promise: Promise
 });
 
-async function enrichCity(city) {
-  const cityPrediction = await getCityPrediction(city.city_name);
-  return city = {
-    country_id: 1,
-    city_name: cityPrediction.structured_formatting.main_text,
-    google_place_id: cityPrediction.place_id
-  };
-}
-
-async function getCityPrediction(city_name) {
-  const query = `${city_name}, ישראל`;
-  const cityPrediction = await getGooglePlacesPrediction(query, '(cities)');
-  if (cityPrediction) {
-    return cityPrediction;
+class AddressUtils {
+  static async validateAddress(addressString) {
+    if (addressString && (addressString.match(/,/g) || []).length == 3 && addressString.endsWith('ישראל')) {
+      const predictions = await this.getGooglePlacesPredictions(addressString);
+      if (!predictions || predictions[0] != addressString) {
+        throw new errors.DomainValidationError('AddressNotMatched', addressString);
+      }
+    }
+    else { throw new errors.DomainValidationError('BadAddressFormat', addressString); }
   }
-  else { throw new errors.DomainValidationError('InvalidCity', city_name); }
-}
 
-async function getGooglePlacesPrediction(input, types) {
-  const autoCompRes = await mapsApiClient.placesAutoComplete({ input, types }).asPromise();
-  const predictions = autoCompRes ? autoCompRes.json.predictions : undefined;
-  if (predictions && predictions.length > 0 && predictions[0].description == input) {
-    return predictions[0];
+  static async getGooglePlacesPredictions(input) {
+    const autoCompRes = await mapsApiClient.placesAutoComplete({ input }).asPromise();
+    const { predictions } = autoCompRes.json;
+    return predictions || [];
   }
-  else { throw new errors.DomainValidationError('AddressNotMatched', input); }
-}
 
-async function getGooglePlacesGeocode(address) {
-  const geocodeResponse = await mapsApiClient.geocode({ address }).asPromise();
-  const { results } = geocodeResponse.json;
-  if (results && results[0]) {
-    return results[0].geometry.location;
+  static async parseCity(cityPart) {
+    const googleLocality = await this.getGooglePlacesLocality(cityPart);
+    return {
+      city_name: googleLocality.structured_formatting.main_text,
+      google_place_id: googleLocality.place_id
+    };
   }
-  else { throw new errors.DomainValidationError('GeocodeResolutionError', address); }
+
+
+  static async getGooglePlacesLocality(cityPart) {
+    const predictions = await this.getGooglePlacesPredictions(cityPart);
+    const cityPrediction = _.find(predictions, (prediction) => { _.includes(prediction.types, 'locality'); });
+    if (cityPrediction) {
+      return cityPrediction;
+    }
+    else { throw new errors.DomainValidationError('InvalidCity', cityPart); }
+  }
+
+  static async getGooglePlacesGeocode(address) {
+    const geocodeResponse = await mapsApiClient.geocode({ address }).asPromise();
+    const { results } = geocodeResponse.json;
+    if (results && results[0]) {
+      return results[0].geometry.location;
+    }
+    else { throw new errors.DomainValidationError('GeocodeResolutionError', address); }
+  }
+
+  static parseStreetAndHouseNumber(streetPart) {
+    const house_number = parseInt(streetPart.split(/\s/).pop());
+    if (house_number && streetPart.length > 0) {
+      return {
+        house_number,
+        street_name: streetPart.join()
+      };
+    }
+    else {
+      throw new errors.DomainValidationError('InvalidStreet', streetPart);
+    }
+  }
+
+  static async addressStringToBuildingObject(addressString) {
+    await this.validateAddress(addressString);
+    const addrParts = addressString.split(',').map((part) => part.trim());
+    const streetAndHouseObj = this.parseStreetAndHouseNumber(addrParts[0]);
+    const cityObj = await this.parseCity(addrParts[1]);
+    const geocode = await this.getGooglePlacesGeocode(addressString);
+
+    return Object.assign(streetAndHouseObj, cityObj, {
+      geolocation: {
+        type: 'Point',
+        coordinates: [geocode.lng, geocode.lat]
+      }
+    });
+  }
 }
 
-async function validateAddress(address) {
-  await getGooglePlacesPrediction(address, 'address');
-}
-
-async function validateAndEnrichBuilding(building) {
-  const addressString = `${building.street_name}, ${building.city.city_name}, ישראל`;
-  await validateAddress(addressString, 'address');
-  const cityObj = await enrichCity(building.city);
-  const geocode = await getGooglePlacesGeocode(addressString);
-  building.city = cityObj;
-  building.geolocation = {
-    type: 'Point',
-    coordinates: [geocode.lng, geocode.lat]
-  };
-
-  return building;
-}
-
-
-module.exports = {
-  validateAndEnrichBuilding
-};
+module.exports = AddressUtils;
