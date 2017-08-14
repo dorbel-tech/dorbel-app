cbf1428711a086cb89140b07f92af80a648a8000
Avner Sorek <avnersorek@users.noreply.github.com>
2017-08-14 17:24:53 +0300

    Feature/koa 2 all services (#1432)
    * apt api to use shared server bootstrap util
    
    * migrated front end to koa2
    
    * migrating ohe api to koa2
    
    * migrating notifications service
    
    * upgrading shared

--- notifications-service/src/server/server.js
+++ notifications-service/src/server/server.js
@@ -1,42 +1,11 @@
 'use strict';
-const koa = require('koa');
-const fleekRouter = require('fleek-router');
-const bodyParser = require('koa-bodyparser');
 const shared = require('dorbel-shared');
-const logger = shared.logger.getLogger(module);
-const swaggerDoc = require('./swagger/swagger');
-const app = koa();
 
-const port = parseInt(process.env.PORT) || 3002;
-const env = process.env.NODE_ENV;
-
-app.use(shared.middleware.errorHandler());
-app.use(shared.middleware.requestLogger());
-app.use(bodyParser());
-
-app.use(function* returnSwagger(next) {
-  if (this.method === 'GET' && this.url === '/swagger') {
-    this.body = swaggerDoc;
-  } else {
-    yield next;
-  }
+const { listen } = shared.utils.serverBootstrap.createApp({
+  defaultPort: 3002,
+  swaggerDocPath: `${__dirname}/swagger/swagger`,
+  controllersPath: `${__dirname}/controllers`
 });
-
-fleekRouter(app, {
-  swagger: swaggerDoc,
-  validate: true,
-  middleware: [ shared.middleware.swaggerModelValidator() ]
-});
-
-function listen() {
-  return new Promise((resolve, reject) => {
-    let server = app.listen(port, function () {
-      logger.info({ port, env }, 'listening');
-      resolve(server);
-    })
-    .on('error', reject);
-  });
-}
 
 module.exports = {
   listen
