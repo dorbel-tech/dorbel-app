4fe0594809b7b8df90fdddbf5599ea8eefa8d615
Idan Langer <lxngxr@gmail.com>
2017-08-16 17:52:06 +0300

    WIP - Still need to handle the multiple building objects in the repository + validate that update still works + write tests

--- apartments-api/src/services/utils/addressUtils.js
+++ apartments-api/src/services/utils/addressUtils.js
@@ -97,4 +97,7 @@ async function validateAndEnrichBuilding(building) {
   else { throw new errors.DomainValidationError('AddressNotMatched', { building, addressString }); }
 }
 
-module.exports = AddressUtils;
+
+module.exports = {
+  parseBuildingObject
+};
