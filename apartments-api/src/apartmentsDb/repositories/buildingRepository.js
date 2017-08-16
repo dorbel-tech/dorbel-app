98557382a4971d2b8852bfd855850a0c1c4e554a
Idan Langer <lxngxr@gmail.com>
2017-08-16 17:52:06 +0300

    WIP - Still need to handle the multiple building objects in the repository + validate that update still works + write tests

--- apartments-api/src/apartmentsDb/repositories/buildingRepository.js
+++ apartments-api/src/apartmentsDb/repositories/buildingRepository.js
@@ -7,27 +7,37 @@ const ValidationError = shared.utils.domainErrors.DomainValidationError;
 
 async function updateOrCreate(building, options = {}) {
   // TODO: add reference to country
+  const city = await db.models.city.findOrCreate({
+    where: {
+      city_name: building.city.city_name,
+      google_place_id: building.city.google_place_id
+    },
+    raw: true
+  });
+
+  building.city = city[0];
+
   let neighborhood;
-  if (building.neighborhood_id) {
+  if (building.neighborhood && building.neighborhood.id) {
     neighborhood = await db.models.neighborhood.findOne({
-      where: { id: building.neighborhood_id },
+      where: { id: building.neighborhood.id },
       raw: true
     });
     if (!neighborhood) {
-      logger.error({ neighborhood_id: building.neighborhood_id }, 'Neighborhood not found!');
+      logger.error({ neighborhood_id: building.neighborhood.id }, 'Neighborhood not found!');
       throw new ValidationError('neighborhood not found', building.neighborhood, 'השכונה לא נמצאה');
     }
 
-    if (building.city_id !== neighborhood.city_id) {
-      logger.error({ city_id: building.city_id, neighborhood_city_id: neighborhood.city_id, neighborhood_id: neighborhood.id }, 'Neighborhood city mismatch!');
+    if (city.id !== neighborhood.city_id) {
+      logger.error({ city_id: city.id, neighborhood_city_id: neighborhood.city_id, neighborhood_id: neighborhood.id }, 'Neighborhood city mismatch!');
       throw new ValidationError('neighborhood city mismatch', building, 'אין התאמה בין עיר לשכונה');
     }
   }
 
   building.neighborhood = neighborhood;
-
+  
   // properties that are not part of the unique constraint but might still need to be updated
-  const nonUniqueProps = Object.assign(_.pick(building, ['geolocation', 'elevator', 'floors']), { neighborhood_id: building.neighborhood_id });
+  const nonUniqueProps = Object.assign(_.pick(building, ['geolocation', 'elevator', 'floors']), { neighborhood_id: building.neighborhood ? building.neighborhood.id : undefined });
 
   const findOrCreateResult = await db.models.building.findOrCreate({
     where: {
