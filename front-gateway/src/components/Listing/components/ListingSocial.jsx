f04cd9685ea1ed24531d83aed89122b80389403b
David Virtser <david@dorbel.com>
2017-08-17 09:30:14 +0300

    sharing using standard Facebook JS SDK

--- front-gateway/src/components/Listing/components/ListingSocial.jsx
+++ front-gateway/src/components/Listing/components/ListingSocial.jsx
@@ -17,10 +17,11 @@ class ListingSocial extends React.Component {
       <Col>
         שתפו חברים שמחפשים:
         <div className="listing-social-share-container">
-          <a className="listing-social-share-item fa fa-facebook-f fb-desktop" href={'https://www.facebook.com/sharer.php?u=' + utils.getShareUrl(currentUrl, 'facebook_share')} target="_blank"></a>
-          <a className="listing-social-share-item fa fa-facebook-f fb-mobile" href={'fb://publish/profile/me?text=' + utils.getShareUrl(currentUrl, 'facebook_share')}></a>
+          <div className="listing-social-share-item fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button" data-size="small" data-mobile-iframe="true"><a className="fb-xfbml-parse-ignore" target="_blank" href={'https://www.facebook.com/sharer/sharer.php?u=' + utils.getShareUrl(currentUrl, 'facebook_share') + '&amp;src=sdkpreparse'}></a></div>
+          <a className="listing-social-share-item fa fa-facebook-f fb-mobile" href={'fb://publish/?text=' + utils.getShareUrl(currentUrl, 'facebook_share')}></a>
           <a className="listing-social-share-item whatsapp fa fa-whatsapp" href={'whatsapp://send?text=היי, ראיתי דירה באתר dorbel שאולי תעניין אותך. ' + utils.getShareUrl(currentUrl, 'whatsapp_share')} data-href={utils.getShareUrl(currentUrl, 'whatsapp_share')} data-text="היי, ראיתי דירה באתר dorbel שאולי תעניין אותך."></a>
           <a className="listing-social-share-item fb-messenger-mobile" href={'fb-messenger://share/?app_id=1651579398444396&link=' + utils.getShareUrl(currentUrl, 'messenger_share')}><Icon iconName="dorbel-icon-social-fbmsg" /></a>
+          <div className="listing-social-share-item fb-send" data-href="https://developers.facebook.com/docs/plugins/"></div>
         </div>
       </Col>
     );
