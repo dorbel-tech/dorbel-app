d824942f3478b124e07701b530b67472f1e2b44d
David Virtser <david@dorbel.com>
2017-08-17 14:37:09 +0300

    removed

--- front-gateway/src/components/Listing/components/ListingSocial.jsx
+++ front-gateway/src/components/Listing/components/ListingSocial.jsx
@@ -18,8 +18,7 @@ class ListingSocial extends React.Component {
         שתפו חברים שמחפשים:
         <div className="listing-social-share-container">
           <div className="listing-social-share-item fb-share-button" data-href={utils.getShareUrl(currentUrl, 'facebook_share')} data-layout="button" data-size="small" data-mobile-iframe="false"><a className="fb-xfbml-parse-ignore" target="_blank" href={'https://www.facebook.com/sharer/sharer.php?u=' + utils.getShareUrl(currentUrl, 'facebook_share', true) + '&amp;src=sdkpreparse'}></a></div>
-          <div className="listing-social-share-item fb-send fb-messenger-desktop" data-href={utils.getShareUrl(currentUrl, 'messenger_share')}></div>
-          <a className="listing-social-share-item fb-messenger-mobile" href={'fb-messenger://share/?app_id=1651579398444396&link=' + utils.getShareUrl(currentUrl, 'messenger_share')}><Icon iconName="dorbel-icon-social-fbmsg" /></a>
+          <div className="listing-social-share-item fb-send" data-href={utils.getShareUrl(currentUrl, 'messenger_share')}></div>
           <a className="listing-social-share-item whatsapp fa fa-whatsapp" href={'whatsapp://send?text=היי, ראיתי דירה באתר dorbel שאולי תעניין אותך. ' + utils.getShareUrl(currentUrl, 'whatsapp_share')} data-href={utils.getShareUrl(currentUrl, 'whatsapp_share')} data-text="היי, ראיתי דירה באתר dorbel שאולי תעניין אותך."></a>
         </div>
       </Col>
