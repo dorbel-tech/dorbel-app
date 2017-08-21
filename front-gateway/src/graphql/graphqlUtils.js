191496a5e85865f29decaf1da507972fd41ca3fa
Avner Sorek <avner.sorek@gmail.com>
2017-08-21 16:08:29 +0300

    removing log

--- front-gateway/src/graphql/graphqlUtils.js
+++ front-gateway/src/graphql/graphqlUtils.js
@@ -1,6 +1,5 @@
 function updateHandler(query, dataFunc) {
   return (proxy, mutationResult) => {
-    console.log(query);
     const previousData = proxy.readQuery({ query });
     const newData = dataFunc(previousData, mutationResult);
     proxy.writeQuery({ query, data: newData });
