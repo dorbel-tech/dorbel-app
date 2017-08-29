6090968f63d68439f538f1f9cbbf9b0c5010d742
Amos Lande <amos@dorbel.com>
2017-08-29 15:20:21 +0300

    Align e2e

--- front-gateway/test/e2e/pages/property.js
+++ front-gateway/test/e2e/pages/property.js
@@ -31,7 +31,7 @@ module.exports = {
           selector: '.listing-future-events>.ohe-card-row'
         }
       }
-    }
+    },
     oheModal: {
       selector: '.modal-dialog',
       elements: {
