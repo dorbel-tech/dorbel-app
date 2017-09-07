91b242cf0b9dbfd69bf15e5c6c806df67c5fad18
Avner Sorek <avner.sorek@gmail.com>
2017-09-07 11:46:45 +0300

    share to facebook groups modal complete !

--- front-gateway/src/components/ShareListingToGroupsModal/ShareListingToGroupsModal.jsx
+++ front-gateway/src/components/ShareListingToGroupsModal/ShareListingToGroupsModal.jsx
@@ -1,42 +1,92 @@
 import React from 'react';
 import autobind from 'react-autobind';
-import { Button } from 'react-bootstrap';
+import { inject } from 'mobx-react';
+import { Button, ButtonGroup, Glyphicon } from 'react-bootstrap';
 import Clipboard from 'clipboard';
+import _ from 'lodash';
+import routesHelper from '~/routesHelper';
+import utils from '~/providers/utils';
+import localStorageHelper from '~/stores/localStorageHelper';
 
-const link = (href, name, members) => (
-  <a href={'https://www.facebook.com/groups/' + href} target="_blank">
-    {name}
-    {members} חברים בקבוצה
-  </a>
-);
+import './ShareListingToGroupsModal.scss';
 
-export default class AddTenantModal extends React.Component {
-  static title = (
-    <div>
-      שתפו את המודעה לדיירים שמחפשים דירה ממש עכשיו!
-    </div>
-  );
+const DONT_SHOW_AGAIN_PREFIX = "share_listing_dont_show_again_";
+
+@inject('appProviders')
+export default class ShareListingToGroupsModal extends React.Component {
+  constructor(props) {
+    super(props);
+    autobind(this);
+  }
+
+  static title = 'שתפו את המודעה לדיירים שמחפשים דירה ממש עכשיו!';
+
+  static shouldShow = (listing) => {
+    const city_name = _.get(listing, 'apartment.building.city.city_name');
+    return (city_name === 'תל אביב יפו' && !localStorageHelper.getItem(DONT_SHOW_AGAIN_PREFIX + listing.id));
+  }
 
   componentDidMount() {
-    new Clipboard('#bar');
+    new Clipboard('.share-listing-to-groups-copy-button');
+  }
+
+  getShareText() {
+    const { listing } = this.props;
+    const website_url = process.env.FRONT_GATEWAY_URL || 'https://app.dorbel.com';
+    const currentUrl = website_url + routesHelper.getPropertyPath(listing);
+    const lease_start = utils.formatDate(listing.lease_start);
+
+    return `להשכרה! דירת ${listing.apartment.rooms} חד' ברחוב ${listing.apartment.building.street_name}
+כניסה ב-${lease_start}, ${listing.apartment.size} מ"ר, שכ"ד ₪${listing.monthly_rent}.
+שתפו / שלחו / תייגו חברים שמחפשים !
+${currentUrl}`;
+  }
+
+  groupLink(href, name, members) {
+    return (
+      <Button href={'https://www.facebook.com/groups/' + href} target="_blank"
+              block className="share-listing-to-groups-group-button">
+        <span className="share-listing-to-groups-group-name">{name}</span><br/>
+        <span className="small-text">{members} חברים בקבוצה</span>
+        <Glyphicon className="pull-left small-text" glyph="chevron-left" />
+      </Button>
+    );
+  }
+
+  closeModal(dontShowAgain) {
+    if (dontShowAgain) {
+      localStorageHelper.setItem(DONT_SHOW_AGAIN_PREFIX + this.props.listing.id, true);
+    }
+    this.props.appProviders.modalProvider.close();
   }
 
   render() {
-
     return (
-      <div>
-        <span>הגיעו במהירות לעשות דיירים איכותיים</span><br/>
-        <span>1 .העתיקו את הטקסט</span>
-        <span>(ניתן לשנות אותו בהמשך)</span><br/>
-        <textarea id="foo" value="זה הטקסט שתרצו לפרסם אחים שלי" />
-        <Button id="bar" data-clipboard-target="#foo">
-          Copy That
-        </Button>
-        <h6>2. שתפו אותו בקבוצות הבאות:</h6>
-        { link('35819517694', 'דירות מפה לאוזן בת"א', '136,000') }
-        { link('dirotTA', 'דירות שלמות ריקות בתל אביב *ללא תיווך', '57,000') }
-
-
+      <div className="share-listing-to-groups-modal-body">
+        <div>
+          <span>הגיעו במהירות לעשות דיירים איכותיים</span>
+        </div>
+        <div className="share-listing-to-groups-section">
+          <b>1 .העתיקו את הטקסט</b><br/>
+          <span className="gray-mid-light-text">(ניתן לשנות אותו בהמשך)</span>
+          <textarea className="share-listing-to-groups-textarea" readOnly rows={4} value={this.getShareText()} />
+          <Button className="pull-left share-listing-to-groups-copy-button"
+                  data-clipboard-target=".share-listing-to-groups-textarea">
+            העתק טקסט
+          </Button>
+        </div>
+        <div className="share-listing-to-groups-section share-listing-to-groups-section-2">
+          <b>2. שתפו אותו בקבוצות הבאות:</b>
+          { this.groupLink('35819517694', 'דירות מפה לאוזן בת"א', '136,000') }
+          { this.groupLink('ApartmentsTelAviv', 'דירות להשכרה ריקות או שותפים בתל אביב', '59,000') }
+          { this.groupLink('dirotTA', 'דירות שלמות ריקות בתל אביב', '57,000') }
+          { this.groupLink('665278950172707', 'דירות מפה לאוזן תל אביב', '55,000') }
+          { this.groupLink('543976922383350', 'secret tel aviv דירות ת"א', '42,000') }
+        </div>
+        <div className="share-listing-to-groups-section share-listing-to-groups-section-3">
+          <Button bsStyle="link" className="underline" onClick={() => this.closeModal(true)}>אל תציג שוב</Button>
+          <Button bsStyle="success" onClick={() => this.closeModal(false)}>בפעם הבאה</Button>
+        </div>
       </div>
     );
   }
