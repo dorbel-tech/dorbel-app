104d4af96020930174f4160684782a8ef3a7aa1d
David Virtser <poison77@gmail.com>
2017-09-10 11:15:51 +0300

    Add Facebook groups sharing for Ramat gan, givataim, hulon, bat yam, rishon #1554

--- front-gateway/src/components/ShareListingToGroupsModal/ShareListingToGroupsModal.jsx
+++ front-gateway/src/components/ShareListingToGroupsModal/ShareListingToGroupsModal.jsx
@@ -22,8 +22,9 @@ export default class ShareListingToGroupsModal extends React.Component {
   static title = 'שתפו את המודעה לדיירים שמחפשים דירה ממש עכשיו!';
 
   static shouldShow = (listing) => {
-    const city_name = _.get(listing, 'apartment.building.city.city_name');
-    return (city_name === 'תל אביב יפו' && !localStorageHelper.getItem(DONT_SHOW_AGAIN_PREFIX + listing.id));
+    const allowedCityNames = [ 'תל אביב יפו', 'רמת גן', 'גבעתיים', 'חולון', 'בת ים' ];
+    const cityName = _.get(listing, 'apartment.building.city.city_name');
+    return (allowedCityNames.indexOf(cityName) !== -1 && !localStorageHelper.getItem(DONT_SHOW_AGAIN_PREFIX + listing.id));
   }
 
   componentDidMount() {
@@ -41,7 +42,7 @@ export default class ShareListingToGroupsModal extends React.Component {
     const currentUrl = website_url + routesHelper.getPropertyPath(listing);
     const lease_start = utils.formatDate(listing.lease_start);
 
-    return `להשכרה דירת ${listing.apartment.rooms} חד' ברחוב ${listing.apartment.building.street_name}. כניסה ב-${lease_start}, ${listing.apartment.size} מ"ר, שכ"ד ₪${listing.monthly_rent}.
+    return `להשכרה דירת ${listing.apartment.rooms} חד' ברחוב ${listing.apartment.building.street_name}, ${listing.apartment.building.city.city_name}. כניסה ב-${lease_start}, ${listing.apartment.size} מ"ר, שכ"ד ₪${listing.monthly_rent}.
 שתפו / שלחו / תייגו חברים שמחפשים!
 לפרטים המלאים של הדירה: ${utils.getShareUrl(currentUrl, "facebook_group_share", false)}`;
   }
@@ -68,6 +69,40 @@ export default class ShareListingToGroupsModal extends React.Component {
     this.props.appProviders.modalProvider.close();
   }
 
+  renderCityGroups() {
+    const { listing } = this.props;
+    const cityName = _.get(listing, 'apartment.building.city.city_name');
+
+    switch(cityName) {
+      case 'תל אביב יפו':
+        return[
+          this.groupLink('35819517694', 'דירות מפה לאוזן בת"א', '136,000'),
+          this.groupLink('ApartmentsTelAviv', 'דירות להשכרה ריקות או שותפים בתל אביב', '59,000'),
+          this.groupLink('dirotTA', 'דירות שלמות ריקות בתל אביב', '57,000'),
+          this.groupLink('665278950172707', 'דירות מפה לאוזן תל אביב', '55,000'),
+          this.groupLink('543976922383350', 'דירות ת"א secret tel aviv', '42,000')
+        ];
+      case 'רמת גן':
+      case 'גבעתיים':
+        return[
+          this.groupLink('246902125410185', 'דירות להשכרה ברמת גן / גבעתיים', '38,000'),
+          this.groupLink('654949551249110', 'בגבעתיים/רמת גן דירות להשכרה', '10,000'),
+          this.groupLink('1504969386441960', 'דירות להשכרה ברמת גן/גבעתיים', '8,000'),
+          this.groupLink('1184689764884216', 'דירות הוגנות להשכרה מפה לאוזן - רמת גן גבעתיים', '6,000'),
+          this.groupLink('1386194455009158', 'דירות להשכרה בגבעתיים רמת גן', '6,000')
+        ];
+      case 'חולון':
+      case 'בת ים':
+        return[
+          this.groupLink('212407068826257', 'דירות להשכרה מפה לאוזן חולון -בת ים -ראשון', '11,000'),
+          this.groupLink('1444243359219168', 'דירות להשכרה באזור חולון בת-ים', '8,000'),
+          this.groupLink('201105170260427', 'דירות  השכרה, חולון,  בת ים, והסביבה', '4,000'),
+          this.groupLink('817575694971912', 'דירות להשכרה (ללא תיווך) בת-ים / חולון', '3,000'),
+          this.groupLink('1494404674165851', 'דירות להשכרה בת ים - חולון', '1,000')
+        ];
+    }
+  }
+
   render() {
     return (
       <div className="share-listing-to-groups-modal-body">
@@ -86,11 +121,7 @@ export default class ShareListingToGroupsModal extends React.Component {
         <div className="share-listing-to-groups-section share-listing-to-groups-section-2">
           <b>2. שתפו אותו בקבוצות הפייסבוק הבאות:</b><br/>
           <span className="gray-mid-light-text">עליכם להיות חברים בקבוצות אלו כדי שתוכלו לפרסם בהם</span>
-          { this.groupLink('35819517694', 'דירות מפה לאוזן בת"א', '136,000') }
-          { this.groupLink('ApartmentsTelAviv', 'דירות להשכרה ריקות או שותפים בתל אביב', '59,000') }
-          { this.groupLink('dirotTA', 'דירות שלמות ריקות בתל אביב', '57,000') }
-          { this.groupLink('665278950172707', 'דירות מפה לאוזן תל אביב', '55,000') }
-          { this.groupLink('543976922383350', 'דירות ת"א secret tel aviv', '42,000') }
+          { this.renderCityGroups() }
         </div>
         <div className="share-listing-to-groups-section share-listing-to-groups-section-3">
           <Button bsStyle="link" className="underline" onClick={() => this.closeModal(true)}>אל תציג שוב</Button>
