95fc13abd6fbdd9ea2ac7de3973897cbedae95e1
David Virtser <poison77@gmail.com>
2017-09-17 18:21:35 +0300

    add delay

--- front-gateway/src/components/Dashboard/MyProperties/PropertyStats.jsx
+++ front-gateway/src/components/Dashboard/MyProperties/PropertyStats.jsx
@@ -34,10 +34,12 @@ class PropertyStats extends Component {
 
   showShareToGroupsModal() {
     if (ShareListingToGroupsModal.shouldShow(this.props.listing)) {
-      this.props.appProviders.modalProvider.showInfoModal({
-        title: ShareListingToGroupsModal.title,
-        body: <ShareListingToGroupsModal listing={this.props.listing} />
-      });
+      setTimeout(() => {
+          this.props.appProviders.modalProvider.showInfoModal({
+            title: ShareListingToGroupsModal.title,
+            body: <ShareListingToGroupsModal listing={this.props.listing} />
+          });
+      }, 3000);
     }
   }
 
