8c1aa2aead8d274ffd51f9e007191da5ab4537cc
Amos Lande <amos@dorbel.com>
2017-10-08 18:05:29 +0300

    MyProfile without formsy WIP

--- front-gateway/src/components/Dashboard/MyProfile/MySettingsFields.jsx
+++ front-gateway/src/components/Dashboard/MyProfile/MySettingsFields.jsx
@@ -17,25 +17,25 @@ class MySettingsFields extends Component {
     return (
       <div>
         <Row className="my-settings-input">
-          אפשר למתעניינים לפנות אלי בהודעות דרך מודעת הדירה שלי באתר
           <Toggle defaultChecked={settings.allow_publisher_messages}
             onChange={this.props.onChange}
             name="allow_publisher_messages"
             icons={false}></Toggle>
+          <label>אפשר למתעניינים לפנות אלי בהודעות דרך מודעת הדירה שלי באתר</label>
         </Row>
         <Row className="my-settings-input">
-          שלחו לי עדכונים על מועדי ביקור חדשים של דירות שאהבתי
           <Toggle defaultChecked={settings.receive_like_related_notifications}
             onChange={this.props.onChange}
             name="receive_like_related_notifications"
             icons={false}></Toggle>
+          <label>שלחו לי עדכונים על מועדי ביקור חדשים של דירות שאהבתי</label>
         </Row>
         <Row className="my-settings-input">
-          שלחו לי עדכונים בנוגע לשירותים חדשים
           <Toggle defaultChecked={settings.receive_newsletter}
             onChange={this.props.onChange}
             name="receive_newsletter"
             icons={false}></Toggle>
+          <label>שלחו לי עדכונים בנוגע לשירותים חדשים</label>
         </Row>
       </div>
     );
